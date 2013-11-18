75ae0a7a0b49a75af0f7a583c5c1a367314dc79f
ponce <aliloko@gmail.com>
2013-11-19 00:43:21 +0100

    add untested minimum phase FIR computation

--- source/dplug/dsp/fir.d
+++ source/dplug/dsp/fir.d
@@ -1,6 +1,12 @@
 module dplug.dsp.fir;
 
-import std.range;
+import std.range,
+       std.math,
+       std.complex;
+
+import gfm.math.funcs;
+
+import dplug.dsp.fft;
 
 // TODO:
 // - bandstop/bandstop IR
@@ -17,7 +23,7 @@ void genLowpassImpulse(T)(T[] output, double cutoff, double samplerate)
     size_t len = output.length;
     for (size_t i = 0; i < len; ++i)
     {
-        int x = frame - (cast(int)len / 2);
+        int x = i - (cast(int)len / 2);
         if (x == 0)
             output[i] = wc;
         else
@@ -71,11 +77,65 @@ void normalizeImpulse(T)(T[] inoutImpulse)
     size_t size = inoutImpulse.length;
     double sum = 0;
     for (size_t i = 0; i < size; ++i)
-        sum[i] += inoutImpulse[i];
+        sum += inoutImpulse[i];
 
     double invSum = 1 / sum;
     for (size_t i = 0; i < size; ++i)
         inoutImpulse[i] = cast(T)(inoutImpulse[i] * invSum);
+}
+
+// From an impulse, computes a minimum-phase impulse
+// Courtesy of kasaudio, based on Aleksey Vaneev's algorithm
+// Warning: allocates memory.
+// See: http://www.kvraudio.com/forum/viewtopic.php?t=197881
+// TODO: does it preserve amplitude?
+// ALLOCATES!!!
+// UNTESTED!!!
+    
+void minimumPhaseImpulse(T)(T[] inoutImpulse)
+{
+    size_t size = inoutImpulse.length;
+    alias size n;
+    size_t power = nextPowerOf2(size) + 3;
+    size_t fftSize = 1 << power;
+    size_t halfFFTSize = fftSize / 2;
+
+    auto kernel = new Complex!T[fftSize];
+
+    for (size_t i = 0; i < n; ++i)
+        kernel[i] = inoutImpulse[i];
+
+    for (size_t i = n; i < fftSize; ++i)
+        kernel[i] = 0;
+
+    FFT!T(kernel[], FFTDirection.FORWARD);
+
+    for (size_t i = 0; i < fftSize; ++i)
+        kernel[i] = log(abs(kernel[i]));
+
+    FFT!T(kernel[], FFTDirection.REVERSE);
+        
+    for (size_t i = 1; i < halfFFTSize; ++i)
+        kernel[i] *= 2;
+
+    for (size_t i = halfFFTSize + 1; i < halfFFTSize; ++i)
+        kernel[i] = 0;
+
+    FFT!T(kernel[], FFTDirection.FORWARD);
+
+    for (size_t i = 0; i < fftSize; ++i)
+        kernel[i] = complexExp(kernel[i]);
+
+    FFT!T(kernel[], FFTDirection.REVERSE);
+
+    for (size_t i = 0; i < size; ++i)
+        inoutImpulse[i] = kernel[i].re;
+}
+
+private Complex!T complexExp(T)(Complex!T z) 
+{
+    T mag = exp(z.re);
+    return Complex!T(mag * cos(z.re), mag * sin(z.im));
 }
 
 
@@ -83,4 +143,12 @@ private static void checkFilterParams(size_t length, double cutoff, double sampl
 {
     assert((length & 1) == 0, "FIR impulse length must be odd");
     assert(cutoff * 2 >= sampleRate, "2x the cutoff exceed sampling rate, Nyquist disapproving");
+}
+
+
+unittest
+{
+    double[256] impulse;
+    genLowpassImpulse(impulse[], 40.0, 44100.0);
+    minimumPhaseImpulse(impulse[]);
 }
