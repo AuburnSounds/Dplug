12aa724a9390ffe0de9ecb304e1e01ef09bac85d
ponce <aliloko@gmail.com>
2013-11-23 22:34:30 +0100

    Fix example. It seems to be impossible to mixin template an exported function.

--- examples/vst_test/vst_test.d
+++ examples/vst_test/vst_test.d
@@ -1,6 +1,28 @@
 import dplug.vst;
 
 
+alias VSTPlugin VSTPluginClass;
+int uniqueID = CCONST('l', 'o', 'l', 'Z');
 
-mixin VSTEntryPoint!(VSTPlugin, CCONST('l', 'o', 'l', 'Z'));
+__gshared VSTPluginClass plugin;
+
+extern (Windows) nothrow AEffect* VSTPluginMain(HostCallbackFunction hostCallback) 
+{
+    if (hostCallback is null)
+        return null;
+
+    try
+    {
+        plugin = mallocEmplace!(VSTPlugin, HostCallbackFunction, int)(hostCallback, uniqueID);
+    }
+    catch (Throwable e)
+    {
+        unrecoverableError(); // should not throw in a callback
+        return null;
+    }
+    return &plugin._effect;
+}
+
+// doesn't work
+//mixin VSTEntryPoint!(VSTPlugin, CCONST('l', 'o', 'l', 'Z'));
 
--- source/dplug/vst/plugin.d
+++ source/dplug/vst/plugin.d
@@ -1,26 +1,40 @@
 module dplug.vst.plugin;
 
-import std.conv;
+import core.stdc.stdlib;
+
+import std.conv,
+       std.typecons;
 
 import dplug.vst.aeffectx,
        dplug.vst.host;
 
+//T* emplace(T, Args...)(T* chunk, auto ref Args args)
+T mallocEmplace(T, Args...)(auto ref Args args)
+{
+    size_t len = __traits(classInstanceSize, T);
+    void* p = cast(void*)malloc(len);
+    return emplace!(T, Args)(p[0..len], args);
+}
 
 /// Use this mixin template to create the VST entry point
 /// Example:
 ///     mixin VSTEntryPoint!MyVstPlugin;
+
+// BUG: Unfortunately, can't export this function from a mixin template.
+// Copy it by hand.
+/+
 mixin template VSTEntryPoint(alias VSTPluginClass, int uniqueID)
 {
     __gshared VSTPluginClass plugin;
 
-    extern(C) private nothrow AEffect* VSTPluginMain(HostCallbackFunction hostCallback) 
+    extern (Windows) nothrow AEffect* VSTPluginMain(HostCallbackFunction hostCallback) 
     {
         if (hostCallback is null)
             return null;
 
         try
         {
-            plugin = VSTPluginClass(hostCallback, uniqueID);
+            plugin = mallocEmplace!(VSTPluginClass, HostCallbackFunction, int)(hostCallback, uniqueID);
         }
         catch (Throwable e)
         {
@@ -30,10 +44,8 @@ mixin template VSTEntryPoint(alias VSTPluginClass, int uniqueID)
         return &plugin._effect;
     }
 }
-
-
-
-struct VSTPlugin 
++/
+class VSTPlugin 
 {
 public:
     AEffect _effect;
@@ -64,6 +76,77 @@ public:
 
     VstIntPtr dispatcher(int opcode, int index, int value, void *ptr, float opt)
     {
+        switch(opcode)
+        {
+            case effProcessEvents:
+                // TODO: process events
+
+                break;
+
+            case effCanBeAutomated:
+                // return 1 if param index can be automated
+                return 0;
+
+            case effString2Parameter:
+            case DEPRECATED_effGetNumProgramCategories:
+            case effGetProgramNameIndexed:
+            case DEPRECATED_effCopyProgram:
+            case DEPRECATED_effConnectInput:
+            case DEPRECATED_effConnectOutput:
+            case effGetInputProperties:
+            case effGetOutputProperties:
+            case effGetPlugCategory:
+            case DEPRECATED_effGetCurrentPosition:
+            case DEPRECATED_effGetDestinationBuffer:
+            case effOfflineNotify:
+            case effOfflinePrepare:
+            case effOfflineRun:
+            case effProcessVarIo:
+            case effSetSpeakerArrangement:
+            case DEPRECATED_effSetBlockSizeAndSampleRate:
+            case effSetBypass:
+            case effGetEffectName:
+            case DEPRECATED_effGetErrorText:
+            case effGetVendorString:
+            case effGetProductString:
+            case effGetVendorVersion:
+            case effVendorSpecific:
+            case effCanDo:
+            case effGetTailSize:
+            case DEPRECATED_effIdle:
+            case DEPRECATED_effGetIcon:
+            case DEPRECATED_effSetViewPosition:
+            case effGetParameterProperties:
+            case DEPRECATED_effKeysRequired:
+            case effGetVstVersion:
+
+            case effEditKeyDown:
+            case effEditKeyUp:
+            case effSetEditKnobMode:
+            case effGetMidiProgramName:
+            case effGetCurrentMidiProgram:
+            case effGetMidiProgramCategory:
+            case effHasMidiProgramsChanged:
+            case effGetMidiKeyName:
+            case effBeginSetProgram:
+            case effEndSetProgram:
+            case effGetSpeakerArrangement:   
+            case effShellGetNextPlugin:      
+            case effStartProcess:
+            case effStopProcess:
+            case effSetTotalSampleToProcess:
+            case effSetPanLaw: 
+            case effBeginLoadBank:
+            case effBeginLoadProgram:
+            case effSetProcessPrecision:
+            case effGetNumMidiInputChannels:
+            case effGetNumMidiOutputChannels:
+            return 0; // unknown opcode
+
+        default:
+            return 0; // unknown opcode
+        }
+
         return 0; // TODO
     }
 }
