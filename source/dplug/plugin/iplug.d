8ba73704f24f9128efa9fe7086fb01d5621da693
ponce <aliloko@gmail.com>
2013-12-01 15:43:10 +0100

    Plugin interface skeleton.

--- source/dplug/plugin/iplug.d
+++ source/dplug/plugin/iplug.d
@@ -3,3 +3,35 @@ module dplug.plugin.iplug;
 
 
 // TODO: define here a plugin interface that could support AU, VST
+
+enum API
+{
+    VST,
+    AUDIO_UNIT
+}
+
+abstract class IParameter
+{
+    void setFromFloat(float x);
+    float getAsFloat();
+}
+
+/// Plugin format wrappers inherit from this base class.
+class IPlugin
+{
+public:
+    this()
+    {
+     //   buildParameter();
+    }
+
+    // get number of parameters
+    size_t getParamCount();
+
+    // get parameter i
+    IParameter getParam(size_t i);
+
+protected:
+
+
+}
