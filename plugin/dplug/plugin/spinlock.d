6766362e0163184c7fff078883d0cd9b9324d44e
ponce <aliloko@gmail.com>
2014-01-12 18:40:53 +0100

    Add spin-locked queue. It will help to implement VST client wrapper.

--- plugin/dplug/plugin/spinlock.d
+++ plugin/dplug/plugin/spinlock.d
@@ -23,14 +23,25 @@ struct Spinlock
         {
             while(!cas(&_state, UNLOCKED, LOCKED))
             {
-                // No wait at all! 
-                // It will consume all CPU until unlocked.
-                // So hurry.
+                //TODO: PAUSE instruction for GDC/LDC (for now it will fail to compile)
+                asm
+                {
+                    rep; nop; // PAUSE instruction, recommended by Intel in busy spin loops
+                }
+
             }
+        }
+
+        /// Returns: true if the spin-lock was locked.
+        bool tryLock() nothrow
+        {
+            return cas(&_state, UNLOCKED, LOCKED);
         }
 
         void unlock() nothrow
         {
+            // TODO: on x86, we don't necessarily need an atomic op if 
+            // _state is on a DWORD address
             atomicStore(_state, UNLOCKED);
         }
     }
@@ -59,3 +70,56 @@ struct Spinlocked(T)
         return current;
     }
 }
+
+
+/// Queue with spinlocks for inter-thread communication.
+/// Support multiple writers, multiple readers.
+/// Should work way better with low contention.
+///
+/// Important: Will crash if the queue is overloaded!
+///            ie. if the producer produced faster than the producer consumes.
+///            In the lack of a lock-free allocator there is not much more we can do.
+final class SpinlockedQueue(T)
+{
+    private
+    {
+        FixedSizeQueue!T _queue;
+        Spinlock _lock;
+    }
+
+    public
+    {
+        /// Creates a new spin-locked queue with fixed capacity.
+        this(size_t capacity)
+        {
+            _queue = new FixedSizeQueue!T(capacity);
+            _lock = Spinlock();
+        }
+
+        /// Pushes an item to the back, block if queue is full.
+        void pushBack(T x)
+        {
+            _lock.lock();
+            _queue.pushBack(x);
+            _lock.unlock();
+        }
+
+        /// Pops an item from the front, block if queue is empty.
+        /// Will crash if the queue is overloaded.
+        bool popFront(out T result)
+        {
+            bool hadItem;
+            _lock.lock();
+            if (_queue.length() != 0)
+            {
+                result = _queue.popFront();
+                hadItem = true;
+            }
+            else
+                hadItem = false;
+            _lock.unlock();
+            return hadItem;
+        }
+    }
+}
+
