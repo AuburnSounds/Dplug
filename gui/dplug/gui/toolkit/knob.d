e808215fe01b6af6f42714835a2e1f9f99d39683
ponce <contact@gamesfrommars.fr>
2015-04-30 15:21:58 +0200

    Ugly value display for knobs

--- gui/dplug/gui/toolkit/knob.d
+++ gui/dplug/gui/toolkit/knob.d
@@ -1,5 +1,6 @@
 module dplug.gui.toolkit.knob;
 
+import std.math;
 import dplug.gui.toolkit.element;
 
 class UIKnob : UIElement
@@ -11,6 +12,7 @@ public:
         super(context);
         _label = label;
         _font = font;
+        _value = 0.5f;
     }
 
     override void preRender(ImageRef!RGBA surface)
@@ -23,16 +25,31 @@ public:
         if (isDragged())
             c = RGBA(150, 150, 80, 255);
 
-        surface.fillRect(_position.min.x, _position.min.y, _position.max.x, _position.max.y, c);
+
+        int centerx = _position.center.x;
+        int centery = _position.center.y;
+        int radius = _position.width / 2;
+        int radius2 = radius / 2;
+        float angle = (_value - 0.5f) * 4.0f;
+        float posEdgeX = centerx + sin(angle) * radius;
+        float posEdgeY = centery - cos(angle) * radius;
+        float posEdgeX2 = centerx + sin(angle) * radius2;
+        float posEdgeY2 = centery - cos(angle) * radius2;
+
+        surface.softCircle(centerx, centery, radius - 1, radius, c);
+        surface.aaLine(posEdgeX, posEdgeY, posEdgeX2, posEdgeY2, RGBA(0,0, 0, 0));
+
         _font.size = 16;
         _font.color = RGBA(220, 220, 220, 255);
         _font.fillText(surface, _label, _position.center.x, _position.max.y + 20);
+
+
     }
 
     // Called when mouse drag this Element.
     override void onMouseDrag(int x, int y, int dx, int dy)
     {
-        // TODO change plugin parameter, or callback something
+        _value = _value + dy * 0.001f;
     }
 
     // Called when mouse drag this Element.
@@ -45,4 +62,6 @@ private:
     string _label;
     Font _font;
     int x = 0;
+
+    float _value; // between 0 and 1
 }
