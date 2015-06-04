9b86bda6d05ae05c9946203deae2cce3570e37ca
ponce <contact@gamesfrommars.fr>
2015-06-04 13:14:13 +0200

    Fix pink noise.

--- dsp/dplug/dsp/noise.d
+++ dsp/dplug/dsp/noise.d
@@ -4,20 +4,20 @@ module dplug.dsp.noise;
 import std.random,
        std.math;
 
-import gfm.math.simplerng;
+import gfm.core.memory;
 
 /// Generates white gaussian noise.
 struct WhiteNoise
 {
 public:
-    void initialize()
+    void initialize() nothrow @nogc
     {
-        _rng.seed(unpredictableSeed());
+        _rng.seed(nogc_unpredictableSeed());
     }
 
-    float next()
+    float next() nothrow @nogc
     {
-        return randNormal!Xorshift32(_rng, 0.0, 1.0);
+        return randNormal!Xorshift32(_rng, 0.0f, 1.0f);
     }
 
 private:
@@ -33,7 +33,7 @@ public:
     enum int PERIOD = 30;
     enum int NOISE_DURATION = 2;
 
-    void initialize(double samplerate)
+    void initialize(double samplerate) nothrow @nogc
     {
         _noise.initialize();
         _increment = 1.0 / samplerate;
@@ -41,7 +41,7 @@ public:
     }
 
     /// Return the next
-    double next()
+    double next() nothrow @nogc
     {
         _counter += _increment;
         while (_counter >= PERIOD)
@@ -64,16 +64,16 @@ private:
 struct Perlin1D
 {
 public:
-    void init(double frequency, double samplerate)
+    void init(double frequency, double samplerate) nothrow @nogc
     {
-        _rng.seed(unpredictableSeed());
+        _rng.seed(nogc_unpredictableSeed());
         _current = 0.0f;
         newGoal();
         _phase = 0.0f;
         _phaseInc = cast(float)(frequency / samplerate);
     }
 
-    float next()
+    float next() nothrow @nogc
     {
         _phase += _phaseInc;
         if (_phase > 1)
@@ -87,14 +87,14 @@ public:
     }
 
 private:
-    static float smootherstep(float x)
+    static float smootherstep(float x) nothrow @nogc
     {
         return x * x * x * (x * (x * 6 - 15) + 10);
     }
 
-    void newGoal()
+    void newGoal() nothrow @nogc
     {
-        _goal = 2 * (uniform(0.0f, 1.0f, _rng) - 0.5f);
+        _goal = 2 * (nogc_uniform_float(0.0f, 1.0f, _rng) - 0.5f);
     }
 
     float _current;
@@ -115,7 +115,7 @@ struct PinkNoise
 {
     void init(double frequency, double samplerate) nothrow @nogc
     {
-        _rng.seed(unpredictableSeed());
+        _rng.seed(nogc_unpredictableSeed());
         contrib[] = 0;
         accum = 0;
 
@@ -127,8 +127,8 @@ struct PinkNoise
 
     float next() nothrow @nogc
     {
-        short int  randu = _rng.uniform(0, 32768);
-        short int  randv = _rng.uniform(-32768, 32768); // [-32768,32767]
+        int randu = nogc_uniform_int(0, 32768, _rng);
+        int  randv = nogc_uniform_int(-32768, 32768, _rng); // [-32768,32767]
 
         // Structured block, at most one update is performed
         for (int n = 0; n < 5; ++n)
@@ -146,7 +146,49 @@ struct PinkNoise
 
 private:
 
-    static immutable int pA[5] = [ 14055, 12759, 10733, 12273, 15716 ];
-    static immutable int pPSUM[5] = [ 22347, 27917, 29523, 29942, 30007 ];
+    static immutable int[5] pA = [ 14055, 12759, 10733, 12273, 15716 ];
+    static immutable int[5] pPSUM = [ 22347, 27917, 29523, 29942, 30007 ];
 }
 
+private
+{
+    // Work-around std.random not being @nogc
+    auto nogc_unpredictableSeed() @nogc nothrow
+    {
+        return assumeNothrowNoGC( (){ return unpredictableSeed(); } )();
+    }
+
+    auto nogc_uniform_int(int min, int max, ref Xorshift32 rng) @nogc nothrow
+    {
+        return assumeNothrowNoGC( (int min, int max, ref Xorshift32 rng)
+                                  { 
+                                      return uniform(min, max, rng); 
+                                  } )(min, max, rng);
+    }
+
+    auto nogc_uniform_float(float min, float max, ref Xorshift32 rng) @nogc nothrow
+    {
+        return assumeNothrowNoGC( (float min, float max, ref Xorshift32 rng)
+                                  { 
+                                      return uniform(min, max, rng); 
+                                  } )(min, max, rng);
+    }
+
+
+    /// Returns: Normal (Gaussian) random sample.
+    /// See_also: Box-Muller algorithm.
+    float randNormal(RNG)(ref RNG rng, float mean = 0.0, float standardDeviation = 1.0) nothrow @nogc 
+    {
+        assert(standardDeviation > 0);
+        double u1;
+
+        do
+        {
+            u1 = nogc_uniform_float(0.0f, 1.0f, rng);
+        } while (u1 == 0); // u1 must not be zero
+        float u2 = nogc_uniform_float(0.0f, 1.0f, rng);
+        float r = sqrt(-2.0 * log(u1));
+        float theta = 2.0 * PI * u2;
+        return mean + standardDeviation * r * sin(theta);
+    }
+}
