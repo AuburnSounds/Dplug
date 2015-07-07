66ea9fc7e4ebeced7dd67dcf7cd5e17ebcf50b59
ponce <aliloko@gmail.com>
2015-07-08 00:29:22 +0200

    Tweak default materials.

--- gui/dplug/gui/materials.d
+++ gui/dplug/gui/materials.d
@@ -14,25 +14,25 @@ struct Material
 {
     RGB albedo;
     ubyte metalness;
-    ubyte roughness;
+    ubyte specular;
 
     // Pre-defined materials
     static Material
-        iron     =    Material(RGB(143, 145, 148), 255),
-        silver   =    Material(RGB(248, 245, 233), 255),
-        aluminum =    Material(RGB(233, 235, 236), 255),
-        gold     =    Material(RGB(255, 195,  86), 255),
-        copper   =    Material(RGB(243, 162, 137), 255),
-        chromium =    Material(RGB(140, 142, 141), 255),
-        nickel   =    Material(RGB(168, 155, 134), 255),
-        titanium =    Material(RGB(138, 127, 114), 255),
-        cobalt   =    Material(RGB(169, 170, 162), 255),
-        platinum =    Material(RGB(171, 162, 218), 255),
-        charcoal =    Material(RGB( 50,  50,  50),  10), 
-        wornAsphalt = Material(RGB( 70,  70,  70),  10), 
-        desertSand =  Material(RGB( 92,  92,  92),  10), 
-        oceanIce =    Material(RGB(142, 142, 142),  10), 
-        freshSnow =   Material(RGB(207, 207, 207),  10); 
+        iron     =    Material(RGB(143, 145, 148), 255, 128),
+        silver   =    Material(RGB(248, 245, 233), 255, 128),
+        aluminum =    Material(RGB(233, 235, 236), 255, 128),
+        gold     =    Material(RGB(255, 195,  86), 255, 128),
+        copper   =    Material(RGB(243, 162, 137), 255, 128),
+        chromium =    Material(RGB(140, 142, 141), 255, 128),
+        nickel   =    Material(RGB(168, 155, 134), 255, 128),
+        titanium =    Material(RGB(138, 127, 114), 255, 128),
+        cobalt   =    Material(RGB(169, 170, 162), 255, 128),
+        platinum =    Material(RGB(171, 162, 218), 255, 128),
+        charcoal =    Material(RGB( 50,  50,  50),  defaultMetalnessDielectric, 64), 
+        wornAsphalt = Material(RGB( 70,  70,  70),  defaultMetalnessDielectric, 64), 
+        desertSand =  Material(RGB( 92,  92,  92),  defaultMetalnessDielectric, 64), 
+        oceanIce =    Material(RGB(142, 142, 142),  defaultMetalnessDielectric, 57), 
+        freshSnow =   Material(RGB(255, 255, 255),  defaultMetalnessDielectric, 50); 
 }
 
 // Sets of recommended values
@@ -41,5 +41,5 @@ enum ushort defaultDepth = 15000;
 enum ushort defaultRoughness = 128;
 enum ushort defaultSpecular = 128; // because everything is shiny
 enum ushort defaultPhysical = 255;
-enum ushort defaultMetalnessDielectric = 10; // ~ 0.04
+enum ushort defaultMetalnessDielectric = 25; // ~ 0.08
 enum ushort defaultMetalnessMetal = 255;
