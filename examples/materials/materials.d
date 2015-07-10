790e50930b9c62774cd271daad30621b8dd13ad2
ponce <contact@gamesfrommars.fr>
2015-07-10 14:39:49 +0200

    Make pretty images in material example.

--- examples/materials/materials.d
+++ examples/materials/materials.d
@@ -4,6 +4,7 @@
 * Authors:   Guillaume Piolat
 */
 import std.math;
+import std.random;
 
 import gfm.image;
 
@@ -69,7 +70,7 @@ public:
     this(MaterialsPlugin client)
     {
         _client = client;
-        super(1024, 512); // initial size
+        super(1024, 1024); // initial size
 
         context.setSkybox( loadImage(cast(ubyte[])(import("skybox.png"))) );        
     }
@@ -87,9 +88,9 @@ public:
         depthMap.fill(L16(defaultDepth));
         diffuseMap.fill(RGBA(186, 186, 186, 0));
 
-        void makeBall(float x, float y, ubyte roughness, Material material)
+        void makeBall(float curvature)(float x, float y, float radius, ushort height, ubyte roughness, ubyte emissive, Material material)
         {
-            RGBA diffuse = RGBA(material.albedo.r, material.albedo.g, material.albedo.b, 0);
+            RGBA diffuse = RGBA(material.albedo.r, material.albedo.g, material.albedo.b, emissive);
 
       //      diffuse = RGBA(255, 128, 128, 0);
 
@@ -97,37 +98,77 @@ public:
             ubyte specular = material.specular;
             ubyte physical = 255;
 
-            roughness = cast(ubyte)(linmap!float(x, 50, 950, 0, 255));
-
-            depthMap.softCircleFloat!1.0f(x, y, 0, 40, L16(65535));
+            depthMap.softCircleFloat!1.4f(x, y, 0, radius, L16(height));
             
-            diffuseMap.softCircleFloat(x, y, 38, 40, diffuse);            
-            materialMap.softCircleFloat(x, y, 38, 40, RGBA(roughness, metalness, specular, physical));
+            diffuseMap.softCircleFloat(x, y, radius- 2, radius, diffuse);            
+            materialMap.softCircleFloat(x, y, radius - 2, radius, RGBA(roughness, metalness, specular, physical));
             
         }
 
-        makeBall( 50, 50, 0, Material.aluminum);
-        makeBall(150, 50, 0, Material.charcoal);
-        makeBall(250, 50, 0, Material.chromium);
-        makeBall(350, 50, 0, Material.cobalt);
-        makeBall(450, 50, 0, Material.copper);
-        makeBall(550, 50, 0, Material.desertSand);
-        makeBall(650, 50, 0, Material.freshSnow);
-        makeBall(750, 50, 0, Material.gold);
-        makeBall(850, 50, 0, Material.iron);
-        makeBall(950, 50, 0, Material.nickel);
+        import gfm.math;
+
+        Random random = Random();
+
+        foreach(ghertg; 0..5000)
+        {
+            float fr = uniform(0.8f, 1.0f);
+            float fg = uniform(0.0f, 0.2f);
+            float fb = uniform(0.3f, 0.4f);
+
+            float radius = 2 + 50.0f * uniform(0.0f, 1.0f) ^^ 2;
+
+            if (uniform(0.0f, 1.0f) < 0.5f)
+            {
+                fr *= 0.4f;
+                fg *= 0.4f;
+                fb *= 0.4f;
+                radius *= 0.5f;
+            }else
+                if (uniform(0.0f, 1.0f) < 0.5f)
+                {
+                    fr *= 2.0f;
+                    fg *= 2.0f;
+                    fb *= 2.0f;
+                    radius *= 0.4f;
+                }
+
+            ubyte r = cast(ubyte)(0.5 + 255.0f * fr);
+            ubyte g = cast(ubyte)(0.5 + 255.0f * fg);
+            ubyte b = cast(ubyte)(0.5 + 255.0f * fb);
+            ubyte metal = cast(ubyte)(0.5 + 255.0f * uniform(0.0f, 1.0f));
+            ubyte emissive = cast(ubyte)(0.5 + 255.0f * uniform(0.0f, 1.0f) ^^ 4);
+            ubyte specular = cast(ubyte)(0.5 + 255.0f * uniform(0.0f, 1.0f));
+            ubyte roughness = cast(ubyte)(0.5 + 255.0f * uniform(0.0f, 1.0f) ^^ 2 );
+
+            Material mat;
+            mat.albedo = ae.utils.graphics.RGB(r, g, b);
+            mat.metalness =  uniform(0.0f, 1.0f) > 0.5 ? 255 : 10;
+            mat.specular = specular;
+
+            float x = randNormal(random, 511.5f, 128.0f);
+            float y = randNormal(random, 511.5f, 128.0f);
+            
+            ushort depth = uniform(0.0f, 1.0f) > 0.5 ? 65535 : 0;
+
+            makeBall!1.0f(x, y, radius, depth, roughness, emissive, mat);
+        }
+/*
+        makeBall!1.0f(150, 150, 60, 65535, 32, Material.aluminum);
+        makeBall!0.3f(100, 170, 60, 0, 128, Material.nickel);
+   */     
+        
+        
+        
+        /*makeBall!1.2f(350, 100, 80, 0, Material.platinum);
+        makeBall!1.3f(400, 100, 60, 0, Material.gold);
+        makeBall!1.4f(500, 100, 80, 0, Material.copper);*/
+/*
         makeBall( 50, 150, 0, Material.oceanIce);
         makeBall(150, 150, 0, Material.platinum);
         makeBall(250, 150, 0, Material.silver);
         makeBall(350, 150, 0, Material.titanium);
-        makeBall(450, 150, 0, Material.wornAsphalt);
-
-        makeBall(550, 150, 0, Material.oceanIce);
-        makeBall(650, 150, 0, Material.platinum);
-        makeBall(750, 150, 0, Material.silver);
-        makeBall(850, 150, 0, Material.titanium);
-        makeBall(950, 150, 0, Material.wornAsphalt);
-
+        makeBall(450, 150, 0, Material.wornAsphalt);*/
+/*
         void makeRect(int x1, int y1, int x2, int y2, ushort depth0, ushort depth1,ubyte roughness, Material material)
         {
             RGBA diffuse = RGBA(material.albedo.r, material.albedo.g, material.albedo.b, 0);
@@ -144,7 +185,7 @@ public:
         for (int i = 0; i < 10; ++i)
         {
             makeRect(i * 90, 250, i * 90 + 70, 270 + i * 20, 0, 65535, 128, Material.iron);
-        }
+        }*/
     }
 }
 
