facb58b1cfdc3caac142076b12e9ff0857601130
Guillaume Piolat <guillaume.piolat@gmail.com>
2015-07-22 11:19:22 +0200

    Implement key events for X11 window

--- window/dplug/window/x11window.d
+++ window/dplug/window/x11window.d
@@ -5,10 +5,11 @@
 */
 module dplug.window.x11window;
 
-
-import gfm.math;
+import core.stdc.config;
 
 import ae.utils.graphics;
+
+import gfm.math;
 
 import dplug.window.window;
 
@@ -18,6 +19,7 @@ version(linux)
     import x11.X;
     import x11.Xutil;
     import x11.Xlib;
+    import x11.keysymdef;
 
     final class X11Window : IWindow
     {
@@ -25,8 +27,10 @@ version(linux)
         bool _terminated = false;
         Display* _display;
         Window _window;    
-        int _screen;    
+        int _screenNumber;
+        Screen* _screen;    
         IWindowListener _listener;
+        XEvent _event;
 
     public:
 
@@ -37,11 +41,57 @@ version(linux)
             if (_display is null)
                 throw new Exception("Cannot open display");
 
-            _screen = DefaultScreen(_display);
-            _window = XCreateSimpleWindow(_display, RootWindow(_display, _screen), 10, 10, width, height, 1, 
-                                          BlackPixel(_display, _screen), 
-                                          WhitePixel(_display, _screen));
-            XSelectInput(_display, _window, ExposureMask | KeyPressMask);
+            _screen = DefaultScreenOfDisplay(_display);
+            _screenNumber = DefaultScreen(_display);
+
+            Window parent;
+            int x, y;
+            if (parentWindow is null)
+            {
+                x = 0;
+                y = 0;
+                parent = RootWindow(_display, _screenNumber);
+            }
+            else
+            {
+                parent = cast(Window)(parentWindow);
+                x = (WidthOfScreen(_screen) - width) / 2;
+                y = (HeightOfScreen(_screen) - height) / 2;
+            }
+
+            c_long eventMask = 
+                KeyPressMask |
+                KeyReleaseMask |
+      /*          ButtonPressMask,
+                ButtonReleaseMask,
+                EnterWindowMask,
+                LeaveWindowMask,
+                PointerMotionMask,
+                PointerMotionHintMask,
+                Button1MotionMask,
+                Button2MotionMask,
+                Button3MotionMask,
+                Button4MotionMask,
+                Button5MotionMask,
+                ButtonMotionMask,
+                KeymapStateMask, */
+                ExposureMask |
+/*                VisibilityChangeMask,
+                StructureNotifyMask,
+                ResizeRedirectMask,
+                SubstructureNotifyMask,
+                SubstructureRedirectMask,
+                FocusChangeMask,
+                PropertyChangeMask,
+                ColormapChangeMask,
+                OwnerGrabButtonMask*/
+                0 ;
+            
+
+            auto black = BlackPixel(_display, _screenNumber);
+
+            _window = XCreateSimpleWindow(_display, parent, x, y, width, height, 1, black, black);
+            XSelectInput(_display, _window, eventMask);
             XMapWindow(_display, _window);
         }
 
@@ -63,15 +113,8 @@ version(linux)
         // Implements IWindow
         override void waitEventAndDispatch()
         {
-            XEvent _xevent;
-            XNextEvent(_display, &_xevent);
-            if (_xevent.type == Expose) 
-            {
-                XFillRectangle(_display, _window, DefaultGC(_display, _screen), 20, 20, 10, 10);
-                //XDrawString(_display, _window, DefaultGC(_display, _screen), 10, 50, msg, strlen(msg));
-            }
-            if (_xevent.type == KeyPress)
-                _terminated = true;
+            XNextEvent(_display, &_event);
+            dispatchEvent(&_event);
         }
 
         override bool terminated()
@@ -81,7 +124,8 @@ version(linux)
 
         override void debugOutput(string s)
         {
-            // TODO
+            import std.stdio;
+            writeln(s); // TODO: something better
         }
 
         override uint getTimeMs()
@@ -91,5 +135,132 @@ version(linux)
             clock_gettime(CLOCK_REALTIME, &time);
             return cast(uint)(time.tv_sec * 1000 + time.tv_nsec / 1_000_000);
         }
+    private:
+
+        void dispatchEvent(XEvent* event)
+        {
+            switch (event.type)
+            {
+                case Expose: 
+                    handleXExposeEvent(&event.xexpose);
+                    break;
+
+                case KeyPress: 
+                    handleXKeyEvent(&event.xkey, false);
+                    break;
+
+                case KeyRelease: 
+                    handleXKeyEvent(&event.xkey, true);
+                    break;
+
+                default:
+                  // ignore
+            }
+        }
+
+        void handleXKeyEvent(XKeyEvent* event, bool release)
+        {
+            if (release)
+                _listener.onKeyUp(translateToKey(event));
+            else   
+                _listener.onKeyDown(translateToKey(event));          
+        }      
+
+        Key translateToKey(XKeyEvent* event)
+        {
+            char[32] buffer;
+            KeySym keysym;
+            XLookupString(event, buffer.ptr, cast(int)buffer.length, &keysym, null);
+
+            switch(keysym)
+            {
+                case XK_space: return Key.space;
+                case XK_Up: return Key.upArrow;
+                case XK_Down: return Key.downArrow;
+                case XK_Left: return Key.leftArrow;
+                case XK_Right: return Key.rightArrow;
+                case XK_KP_0: return Key.digit0;
+                case XK_KP_1: return Key.digit1;
+                case XK_KP_2: return Key.digit2;
+                case XK_KP_3: return Key.digit3;
+                case XK_KP_4: return Key.digit4;
+                case XK_KP_5: return Key.digit5;
+                case XK_KP_6: return Key.digit6;
+                case XK_KP_7: return Key.digit7;
+                case XK_KP_8: return Key.digit8;
+                case XK_KP_9: return Key.digit9;
+                case XK_Return: return Key.enter;
+                case XK_Escape: return Key.escape;
+                default:
+                    return Key.unsupported;
+            }
+        }
+
+        void handleXExposeEvent(XExposeEvent* event)
+        {
+            XFillRectangle(_display, _window, DefaultGC(_display, _screenNumber), 20, 20, 10, 10);
+        }
     }
 }
+
+
+/+
+
+// Receiving commands from a window
+interface IWindowListener
+{
+    /// Called on mouse click.
+    /// Returns: true if the event was handled.
+    bool onMouseClick(int x, int y, MouseButton mb, bool isDoubleClick, MouseState mstate);
+
+    /// Called on mouse button release
+    /// Returns: true if the event was handled.
+    bool onMouseRelease(int x, int y, MouseButton mb, MouseState mstate);
+
+    /// Called on mouse wheel movement
+    /// Returns: true if the event was handled.
+    bool onMouseWheel(int x, int y, int wheelDeltaX, int wheelDeltaY, MouseState mstate);
+
+    /// Called on mouse movement (might not be within the window)
+    void onMouseMove(int x, int y, int dx, int dy, MouseState mstate);
+
+    /// Called on keyboard press.
+    /// Returns: true if the event was handled.
+    bool onKeyDown(Key key);
+
+    /// Called on keyboard release.
+    /// Returns: true if the event was handled.
+    bool onKeyUp(Key up);
+
+    /// An image you have to draw to, or return that nothing has changed.
+    /// The size of this image is given before-hand by onResized.
+    /// recomputeDirtyAreas() MUST have been called before.
+    /// `swapRB` is true when it is expected rendering will swap red and blue channel.
+    void onDraw(ImageRef!RGBA wfb, bool swapRB);
+
+    /// The drawing area size has changed.    
+    /// Always called at least once before onDraw.
+    void onResized(int width, int height);
+
+    /// Recompute internally what needs be done for the next onDraw.
+    /// This function MUST be called before calling `onDraw` and `getDirtyRectangle`.
+    /// This method exists to allow the Window to recompute these draw lists less.
+    /// And because cache invalidation was easier on user code than internally in the UI.
+    void recomputeDirtyAreas();
+
+    /// Returns: Minimal rectangle that contains dirty UIELement in UI + their graphical extent.
+    ///          Empty box if nothing to update.
+    /// recomputeDirtyAreas() MUST have been called before.
+    box2i getDirtyRectangle();
+
+    /// Returns: true if a control must be redrawn.
+    bool isUIDirty();
+
+    /// Called whenever mouse capture was canceled (ALT + TAB, SetForegroundWindow...)
+    void onMouseCaptureCancelled();
+
+    /// Must be called periodically (ideally 60 times per second but this is not mandatory).
+    /// `time` must refer to the window creation time.
+    void onAnimate(double dt, double time);
+}
++/
