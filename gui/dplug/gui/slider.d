1a9bd719139f68c6481c635fbedf30b046933be6
ponce <contact@gamesfrommars.fr>
2016-03-16 17:39:21 +0100

    More customizable slider.

--- gui/dplug/gui/slider.d
+++ gui/dplug/gui/slider.d
@@ -10,9 +10,31 @@ import dplug.gui.element;
 import dplug.gui.drawex;
 import dplug.client.params;
 
+
+enum HandleStyle
+{
+    shapeW,
+    shapeV,
+    shapeA,
+    shapeBlock
+}
+
 class UISlider : UIElement, IParameterListener
 {
 public:
+
+    // Trail customization
+    L16 trailDepth = L16(30000);
+    RGBA unlitTrailDiffuse = RGBA(130, 90, 45, 5);
+    RGBA litTrailDiffuse = RGBA(240, 165, 102, 130);
+    float trailWidth = 0.2f;
+
+    // Handle customization
+    HandleStyle handleStyle = HandleStyle.shapeW;
+    float handleHeightRatio = 0.25f;
+    float handleWidthRatio = 0.7f;
+    RGBA handleDiffuse = RGBA(248, 245, 233, 16);
+    RGBA handleMaterial = RGBA(0, 255, 128, 255);
 
     this(UIContext context, FloatParameter param)
     {
@@ -21,6 +43,7 @@ public:
         _param.addListener(this);
         _sensivity = 1.0f;
         _initialized = true;
+         _pushedAnimation = 0;
         clearCrosspoints();
     }
 
@@ -46,21 +69,41 @@ public:
         return _sensivity = sensivity;
     }
 
+    override void onAnimate(double dt, double time)
+    {
+        float target = isDragged() ? 1 : 0;
+        float newAnimation = lerp(_pushedAnimation, target, 1.0 - exp(-dt * 30));
+        if (abs(newAnimation - _pushedAnimation) > 0.001f)
+        {
+            _pushedAnimation = newAnimation;
+            setDirty();
+        }
+    }
+
+    // Warning: does not respect dirtyRects!
     override void onDraw(ImageRef!RGBA diffuseMap, ImageRef!L16 depthMap, ImageRef!RGBA materialMap, box2i[] dirtyRects)
     {
         int width = _position.width;
         int height = _position.height;
+        int handleHeight = cast(int)(0.5f + this.handleHeightRatio * height * (1 - 0.12f * _pushedAnimation));
+        int handleWidth = cast(int)(0.5f + this.handleWidthRatio * width * (1 - 0.06f * _pushedAnimation));
+        int trailWidth = cast(int)(0.5f + width * this.trailWidth);
+
+        int handleHeightUnpushed = cast(int)(0.5f + this.handleHeightRatio * height);
+        int trailMargin = cast(int)(0.5f + (handleHeightUnpushed - trailWidth) * 0.5f);
+        if (trailMargin < 0) 
+            trailMargin = 0;
+
+        int trailX = cast(int)(0.5 + (width - trailWidth) * 0.5f);
+        int trailHeight = height - 2 * trailMargin;
 
         // The switch is in a subrect
 
-        box2i holeRect =  box2i ( cast(int)(0.5f + width * 0.4f), 2, cast(int)(0.5f + width * (1-0.4f)), height - 2 );
+        box2i holeRect =  box2i(trailX, trailMargin, trailX + trailWidth, trailMargin + trailHeight);
 
         float value = _param.getNormalized();
 
-        int handleHeight = (height + 2) / 4;
-        int handleWidth = cast(int)(0.5f + width * 0.7f);
-
-        int posX = cast(int)(0.5f + (width - handleWidth) / 2);
+        int posX = cast(int)(0.5f + (width - handleWidth) * 0.5f);
         int posY = cast(int)(0.5f + (1 - value) * (height - handleHeight));
         assert(posX >= 0);
         assert(posY >= 0);
@@ -73,21 +116,58 @@ public:
             box2i holeBlack = box2i(holeRect.min.x, holeRect.min.y, holeRect.max.x, std.algorithm.max(holeRect.min.y, posY - 1));
             box2i holeLit = box2i(holeRect.min.x, std.algorithm.min(holeRect.max.y, posY + handleHeight), holeRect.max.x, holeRect.max.y);
 
-            diffuseMap.crop(holeBlack).fill(RGBA(150, 40, 20, 8));
-            diffuseMap.crop(holeLit).fill(RGBA(230, 80, 43, 192));
-            depthMap.crop(holeRect).fill(L16(30000));
+            diffuseMap.cropImageRef(holeBlack).fill(unlitTrailDiffuse);
+
+            // lit trail is two times brighter when dragged
+            RGBA litTrail = litTrailDiffuse;
+            if (isDragged)
+            {
+                litTrail.a = cast(ubyte) std.algorithm.max(255, 2 * litTrail.a);                    
+            }
+
+            diffuseMap.cropImageRef(holeLit).fill(litTrail);
+            depthMap.cropImageRef(holeRect).fill(trailDepth);
         }
 
         // Paint handle of slider
+        int emissive = handleDiffuse.a;
+        if (isMouseOver)
+            emissive += 60;
+        if (isDragged)
+            emissive += 90;
+        if (emissive > 255)
+            emissive = 255;
+
+        RGBA handleDiffuseLit = RGBA(handleDiffuse.r, handleDiffuse.g, handleDiffuse.b, cast(ubyte)emissive);        
+
+        diffuseMap.cropImageRef(handleRect).fill(handleDiffuseLit);
+
+        if (handleStyle == HandleStyle.shapeV)
         {
-            ubyte emissive = 16;
-            if (isDragged || isMouseOver)
-                emissive = 64;
+            auto c0 = L16(20000);
+            auto c1 = L16(50000);
 
-            Material handleMat = Material.silver;
+            int h0 = handleRect.min.y;
+            int h1 = handleRect.center.y;
+            int h2 = handleRect.max.y;
 
-            diffuseMap.crop(handleRect).fill(handleMat.diffuse(emissive));
+            verticalSlope(depthMap, box2i(handleRect.min.x, h0, handleRect.max.x, h1), c0, c1);
+            verticalSlope(depthMap, box2i(handleRect.min.x, h1, handleRect.max.x, h2), c1, c0);
+        }
+        else if (handleStyle == HandleStyle.shapeA)
+        {
+            auto c0 = L16(50000);
+            auto c1 = L16(20000);
 
+            int h0 = handleRect.min.y;
+            int h1 = handleRect.center.y;
+            int h2 = handleRect.max.y;
+
+            verticalSlope(depthMap, box2i(handleRect.min.x, h0, handleRect.max.x, h1), c0, c1);
+            verticalSlope(depthMap, box2i(handleRect.min.x, h1, handleRect.max.x, h2), c1, c0);
+        }
+        else if (handleStyle == HandleStyle.shapeW)
+        {
             auto c0 = L16(15000);
             auto c1 = L16(65535);
             auto c2 = L16(51400);
@@ -102,9 +182,13 @@ public:
             verticalSlope(depthMap, box2i(handleRect.min.x, h1, handleRect.max.x, h2), c1, c2);
             verticalSlope(depthMap, box2i(handleRect.min.x, h2, handleRect.max.x, h3), c2, c1);
             verticalSlope(depthMap, box2i(handleRect.min.x, h3, handleRect.max.x, h4), c1, c0);
-
-            materialMap.crop(handleRect).fill(handleMat.material(0));
         }
+        else if (handleStyle == HandleStyle.shapeBlock)
+        {
+            depthMap.cropImageRef(handleRect).fill(L16(50000));
+        }
+
+        materialMap.cropImageRef(handleRect).fill(handleMaterial);
     }
 
     override bool onMouseClick(int x, int y, int button, bool isDoubleClick, MouseState mstate)
@@ -200,6 +284,8 @@ protected:
     /// Sensivity: given a mouse movement in 100th of the height of the knob,
     /// how much should the normalized parameter change.
     float _sensivity;
+
+    float  _pushedAnimation;
 
     bool _initialized; // destructor flag
 
