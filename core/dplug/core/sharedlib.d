f34fa485a92608bef601290f435cede477209da6
ponce <contact@gamesfrommars.fr>
2016-11-01 23:44:53 +0100

    Fix module name

--- core/dplug/core/sharedlib.d
+++ core/dplug/core/sharedlib.d
@@ -25,7 +25,7 @@ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 
 */
-module derelict.util.sharedlib;
+module dplug.core.sharedlib;
 
 import dplug.core.nogc;
 import dplug.core.alignedbuffer;
