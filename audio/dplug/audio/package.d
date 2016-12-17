0edec92d1d7fd65b5b4e93a20cad28e7754416a2
Guillaume Piolat <guillaume.piolat@gmail.com>
2016-12-17 02:05:36 +0100

    second attempt at DbI

---
+++ audio/dplug/audio/package.d
@@ -0,0 +1,8 @@
+/**
+ * Copyright: Copyright Auburn Sounds 2016
+ * License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+ * Authors:   Guillaume Piolat
+ */
+module dplug.audio;
+
+public import dplug.audio.sound;
