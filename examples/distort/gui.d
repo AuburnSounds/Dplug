9cfe47b44cd676f6108663509f549f94e29eef13
ponce <contact@gamesfrommars.fr>
2017-08-14 17:38:57 +0200

    Update the GUI example to make it prettier.

---
+++ examples/distort/gui.d
@@ -0,0 +1,128 @@
+/**
+* Copyright: Copyright Auburn Sounds 2015-2017
+* License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+* Authors:   Guillaume Piolat
+*/
+module gui;
+
+import std.math;
+
+import gfm.math;
+import dplug.gui;
+import dplug.client;
+
+import main;
+
+// Plugin GUI, based on PBRBackgroundGUI.
+// If you don't want to use PBR, you not inherit from it.
+class DistortGUI : PBRBackgroundGUI!("basecolor.jpg", "emissive.png", "material.png",
+                                     "physical.png", "depth.png", "skybox.jpg",
+
+                                     // Enter here the absolute path to the gfx directory.
+                                     // This will allow to reload images at debug-time with the press of ENTER.
+                                     `C:\Users\myuser\Products\distort\gfx\`)
+{
+public:
+nothrow:
+@nogc:
+
+    DistortClient _client;
+
+    UISlider inputSlider;
+    UIKnob driveKnob;
+    UISlider outputSlider;
+    UIOnOffSwitch onOffSwitch;
+    UIBargraph inputBargraph, outputBargraph;
+
+    Font _font;
+
+    this(DistortClient client)
+    {
+        _client = client;
+        super(620, 330); // size
+
+
+        // Note: PBRCompositor default lighting might change in a future version (increase of light to allow white plastics).
+        //       So we keep the value.
+        PBRCompositor comp = cast(PBRCompositor)compositor;        
+        comp.light1Color = vec3f(0.26, 0.24, 0.22f) * 0.98f;
+        comp.light2Dir = vec3f(-0.5f, 1.0f, 0.23f).normalized;
+        comp.light2Color = vec3f(0.36, 0.38f, 0.40) * 1.148;
+        comp.light3Dir = vec3f(0.0f, 1.0f, 0.1f).normalized;
+        comp.light3Color = vec3f(0.2f, 0.2f, 0.2f) * 0.84f;
+        comp.ambientLight = 0.042f;
+        comp.skyboxAmount = 0.56f;
+
+        // Optional global color correction.
+        // Very useful at the end of the UI creating process.
+        {   
+            mat3x4f colorCorrectionMatrix = mat3x4f(- 0.07f, 1.0f , 1.15f, 0.03f,
+                                                    + 0.01f, 0.93f, 1.16f, 0.08f,
+                                                    + 0.0f , 1.0f , 1.10f, -0.01f);
+            comp.setLiftGammaGainContrastRGB(colorCorrectionMatrix);
+        }
+
+        // Sets the number of pixels recomputed around dirtied controls.
+        // This is a tradeoff between Emissive light accuracy and speed.
+        // A typical value is between 15 and 30 pixels.
+        setUpdateMargin(30);
+
+        // All resources are bundled as a string import.
+        // You can avoid resource compilers that way.
+        // The only cost is that each resource is in each binary, this creates overhead with 
+        _font = mallocNew!Font(cast(ubyte[])( import("VeraBd.ttf") ));
+
+        // Builds the UI hierarchy
+        // Note: when Dplug has resizeable UI, all positionning is going 
+        // to move into a reflow() override.
+        // Meanwhile, we hardcode each position.
+
+        RGBA litTrailDiffuse = RGBA(151, 119, 255, 100);
+        RGBA unlitTrailDiffuse = RGBA(81, 54, 108, 0);
+
+        // Add knob
+        addChild(driveKnob = mallocNew!UIKnob(context(), cast(FloatParameter) _client.param(paramDrive)));
+        driveKnob.position = box2i.rectangle(250, 140, 120, 120);
+        driveKnob.knobRadius = 0.65f;
+        driveKnob.knobDiffuse = RGBA(255, 255, 238, 0);
+        driveKnob.knobMaterial = RGBA(0, 255, 128, 255);
+        driveKnob.numLEDs = 15;
+        driveKnob.litTrailDiffuse = litTrailDiffuse;
+        driveKnob.unlitTrailDiffuse = unlitTrailDiffuse;
+        driveKnob.LEDDiffuseLit = RGBA(40, 40, 40, 100);
+        driveKnob.LEDDiffuseUnlit = RGBA(40, 40, 40, 0);
+        driveKnob.LEDRadiusMin = 0.06f;
+        driveKnob.LEDRadiusMax = 0.06f;
+
+        // Add sliders
+        addChild(inputSlider = mallocNew!UISlider(context(), cast(FloatParameter) _client.param(paramInput)));
+        inputSlider.position = box2i.rectangle(190, 132, 30, 130);
+        inputSlider.litTrailDiffuse = litTrailDiffuse;
+        inputSlider.unlitTrailDiffuse = unlitTrailDiffuse;
+
+        addChild(outputSlider = mallocNew!UISlider(context(), cast(FloatParameter) _client.param(paramOutput)));
+        outputSlider.position = box2i.rectangle(410, 132, 30, 130);
+        outputSlider.litTrailDiffuse = litTrailDiffuse;
+        outputSlider.unlitTrailDiffuse = unlitTrailDiffuse;
+
+        // Add switch
+        addChild(onOffSwitch = mallocNew!UIOnOffSwitch(context(), cast(BoolParameter) _client.param(paramOnOff)));
+        onOffSwitch.position = box2i.rectangle(90, 177, 30, 40);
+        onOffSwitch.diffuseOn = litTrailDiffuse;
+        onOffSwitch.diffuseOff = unlitTrailDiffuse;
+
+        // Add bargraphs
+        addChild(inputBargraph = mallocNew!UIBargraph(context(), 2, -80.0f, 6.0f));
+        inputBargraph.position = box2i.rectangle(150, 132, 30, 130);
+        addChild(outputBargraph = mallocNew!UIBargraph(context(), 2, -80.0f, 6.0f));
+        outputBargraph.position = box2i.rectangle(450, 132, 30, 130);
+        static immutable float[2] startValues = [0.0f, 0.0f];
+        inputBargraph.setValues(startValues);
+        outputBargraph.setValues(startValues);
+    }
+
+    ~this()
+    {
+        _font.destroyFree();
+    }
+}
---
+++ examples/distort/main.d
@@ -0,0 +1,196 @@
+/**
+* Copyright: Copyright Auburn Sounds 2015-2017
+* License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+* Authors:   Guillaume Piolat
+*/
+module main;
+
+import std.math;
+
+import dplug.core,
+       dplug.client,
+       dplug.dsp,
+       dplug.gui;
+
+import gui;
+
+mixin(DLLEntryPoint!());
+
+version(VST)
+{
+    import dplug.vst;
+    mixin(VSTEntryPoint!DistortClient);
+}
+
+version(AU)
+{
+    import dplug.au;
+    mixin(AUEntryPoint!DistortClient);
+}
+
+enum : int
+{
+    paramInput,
+    paramDrive,
+    paramOutput,
+    paramOnOff,
+}
+
+
+/// Example mono/stereo distortion plugin.
+final class DistortClient : dplug.client.Client
+{
+public:
+nothrow:
+@nogc:
+
+    this()
+    {
+    }
+
+    override PluginInfo buildPluginInfo()
+    {
+        // Plugin info is parsed from plugin.json here at compile time
+        static immutable PluginInfo pluginInfo = parsePluginInfo(import("plugin.json"));
+        return pluginInfo;
+    }
+
+    // This is an optional overload, default is zero parameter.
+    // Caution when adding parameters: always add the indices
+    // in the same order than the parameter enum.
+    override Parameter[] buildParameters()
+    {
+        auto params = makeVec!Parameter();
+        params.pushBack( mallocNew!GainParameter(paramInput, "input", 6.0, 0.0) );
+        params.pushBack( mallocNew!LinearFloatParameter(paramDrive, "drive", "%", 1.0f, 2.0f, 1.0f) );
+        params.pushBack( mallocNew!GainParameter(paramOutput, "output", 6.0, 0.0) );
+        params.pushBack( mallocNew!BoolParameter(paramOnOff, "on/off", true) );
+        return params.releaseData();
+    }
+
+    override LegalIO[] buildLegalIO()
+    {
+        auto io = makeVec!LegalIO();
+        io.pushBack(LegalIO(1, 1));
+        io.pushBack(LegalIO(1, 2));
+        io.pushBack(LegalIO(2, 1));
+        io.pushBack(LegalIO(2, 2));
+        return io.releaseData();
+    }
+
+    // This override is optional, the default implementation will
+    // have one default preset.
+    override Preset[] buildPresets() nothrow @nogc
+    {
+        auto presets = makeVec!Preset();
+        presets.pushBack( makeDefaultPreset() );
+
+        static immutable float[] silenceParams = [0.0f, 0.0f, 0.0f, 1.0f, 0];
+        presets.pushBack( mallocNew!Preset("Silence", silenceParams) );
+
+        static immutable float[] fullOnParams = [1.0f, 1.0f, 0.4f, 1.0f, 0];
+        presets.pushBack( mallocNew!Preset("Full-on", fullOnParams) );
+        return presets.releaseData();
+    }
+
+    // This override is also optional. It allows to split audio buffers in order to never
+    // exceed some amount of frames at once.
+    // This can be useful as a cheap chunking for parameter smoothing.
+    // Buffer splitting also allows to allocate statically or on the stack with less worries.
+    override int maxFramesInProcess() const //nothrow @nogc
+    {
+        return 512;
+    }
+
+    override void reset(double sampleRate, int maxFrames, int numInputs, int numOutputs) nothrow @nogc
+    {
+        // Clear here any state and delay buffers you might have.
+
+        assert(maxFrames <= 512); // guaranteed by audio buffer splitting
+
+        foreach(channel; 0..2)
+        {
+            _inputRMS[channel].initialize(sampleRate);
+            _outputRMS[channel].initialize(sampleRate);
+        }
+    }
+
+    override void processAudio(const(float*)[] inputs, float*[]outputs, int frames,
+                               TimeInfo info) nothrow @nogc
+    {
+        assert(frames <= 512); // guaranteed by audio buffer splitting
+
+        int numInputs = cast(int)inputs.length;
+        int numOutputs = cast(int)outputs.length;
+
+        int minChan = numInputs > numOutputs ? numOutputs : numInputs;
+
+        float inputGain = deciBelToFloat(readFloatParamValue(paramInput));
+        float drive = readFloatParamValue(paramDrive);
+        float outputGain = deciBelToFloat(readFloatParamValue(paramOutput));
+
+        bool enabled = readBoolParamValue(paramOnOff);
+
+        float[2] RMS = 0;
+
+        if (enabled)
+        {
+            for (int chan = 0; chan < minChan; ++chan)
+            {
+                for (int f = 0; f < frames; ++f)
+                {
+                    float inputSample = inputGain * 2.0 * inputs[chan][f];
+
+                    // Feed the input RMS computation
+                    _inputRMS[chan].nextSample(inputSample);
+
+                    // Distort signal
+                    float distorted = tanh(inputSample * drive) / drive;
+                    float outputSample = outputGain * distorted;
+                    outputs[chan][f] = outputSample;
+
+                    // Feed the output RMS computation
+                    _outputRMS[chan].nextSample(outputSample);
+                }
+            }
+        }
+        else
+        {
+            // Bypass mode
+            for (int chan = 0; chan < minChan; ++chan)
+                outputs[chan][0..frames] = inputs[chan][0..frames];
+        }
+
+        // fill with zero the remaining channels
+        for (int chan = minChan; chan < numOutputs; ++chan)
+            outputs[chan][0..frames] = 0; // D has array slices assignments and operations
+
+        // Update RMS meters from the audio callback
+        // The IGraphics object must be acquired and released, so that it does not
+        // disappear under your feet
+        if (DistortGUI gui = cast(DistortGUI) graphicsAcquire())
+        {
+            float[2] inputLevels;
+            inputLevels[0] = floatToDeciBel(_inputRMS[0].RMS());
+            inputLevels[1] = minChan >= 1 ? floatToDeciBel(_inputRMS[1].RMS()) : inputLevels[0];
+            gui.inputBargraph.setValues(inputLevels);
+
+            float[2] outputLevels;
+            outputLevels[0] = floatToDeciBel(_outputRMS[0].RMS());
+            outputLevels[1] = minChan >= 1 ? floatToDeciBel(_outputRMS[1].RMS()) : outputLevels[0];
+            gui.outputBargraph.setValues(outputLevels);
+
+            graphicsRelease();
+        }
+    }
+
+    override IGraphics createGraphics()
+    {
+        return mallocNew!DistortGUI(this);
+    }
+
+private:
+    CoarseRMS!float[2] _inputRMS;
+    CoarseRMS!float[2] _outputRMS;
+}
+
