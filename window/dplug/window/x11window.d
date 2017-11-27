8e31944feb82cd57bde9651047ba8f2df1a2a04a
Guillaume Piolat <contact@auburnsounds.com>
2017-11-27 19:03:12 +0100

    Remove DumbSlowNoGCMap in favor of dplug.core.map.Map

--- window/dplug/window/x11window.d
+++ window/dplug/window/x11window.d
@@ -30,6 +30,8 @@ nothrow:
 
 version(linux):
 
+import dplug.core.map;
+
 import derelict.x11.X;
 import derelict.x11.Xlib;
 import derelict.x11.keysym;
@@ -49,7 +51,7 @@ __gshared size_t _white_pixel, _black_pixel; // TODO: could be made a field with
 __gshared int _screen;                       // TODO: could be made a field without questions
 
 // Reverse mapping
-__gshared DumbSlowNoGCMap!(Window, X11Window) x11WindowMapping;
+__gshared Map!(Window, X11Window) x11WindowMapping;
 
 // This is an extension to X11, almost always should exist on modern systems
 // If it becomes a problem, version out its usage, it'll work just won't be as nice event wise
@@ -94,6 +96,8 @@ nothrow:
 
         _windowId = assumeNoGC(&XCreateSimpleWindow)(_display, _parentWindowId, x, y, width, height, 0, 0, _black_pixel);
         assumeNoGC(&XStoreName)(_display, _windowId, cast(char*)" ".ptr);
+
+        // Note: this will create the map lazily
         x11WindowMapping[_windowId] = this;
 
         //
@@ -225,7 +229,8 @@ nothrow:
     //Called on a separate thread to dispatch events and redraw the UI
     void asyncEventHandling() nothrow @nogc
     {
-        while(x11WindowMapping.haveAValue)
+        // TODO: this thread termination test is racey
+        while(!x11WindowMapping.empty)
         {
             waitEventAndDispatch();
             redraw();
@@ -510,122 +515,3 @@ MouseState mouseStateFromX11(uint state) {
         (state & Mod1Mask) == Mod1Mask);
 }
 
-/**
- * A simple @nogc map, that also happens to be quite dumb.
- *
- * It is used for X11 mapping of window id's to instances,
- * if you need something like this for anything more serious, replace!
- * Or look into EMSI's containers.
- *
- */
-
-/// A dumb slow @nogc map implementation. You probably don't want to use this...
-/// No seriously, its probably worse than O(n)!
-struct DumbSlowNoGCMap(K, V)
-{
-    import std.typecons : Nullable;
-
-    private
-    {
-        Nullable!K[] keys_;
-        V[] values_;
-    }
-
-@nogc:
-
-    @disable
-    this(this);
-
-    ~this()
-    {
-        freeSlice(keys_);
-        freeSlice(values_);
-    }
-
-    V opIndex(K key)
-    {
-        foreach(i, ref k; keys_)
-        {
-            if (!k.isNull && k == key)
-                return values_[i];
-        }
-
-        return V.init;
-    }
-
-    void opIndexAssign(V value, K key)
-    {
-        if (keys_.length == 0)
-        {
-            keys_ = mallocSlice!(Nullable!K)(8);
-            values_ = mallocSlice!V(8);
-            keys_[0] = key;
-            values_[0] = value;
-
-            keys_[1 .. $] = Nullable!K.init;
-
-            return;
-        }
-        else
-        {
-            foreach(i, ref k; keys_)
-            {
-                if (!k.isNull && k == key)
-                {
-                    values_[i] = value;
-                    return;
-                }
-            }
-
-            foreach(i, ref k; keys_)
-            {
-                if (k.isNull)
-                {
-                    k = key;
-                    values_[i] = value;
-                    return;
-                }
-            }
-        }
-
-        Nullable!K[] newKeys = mallocSlice!(Nullable!K)(keys_.length+8);
-        V[] newValues = mallocSlice!V(values_.length+8);
-        newKeys[0 .. $-8] = keys_;
-        newValues[0 .. $-8] = values_;
-
-        newKeys[$-7] = key;
-        newKeys[$-6 .. $] = Nullable!K.init;
-        newValues[$-7] = value;
-
-        freeSlice(keys_);
-        freeSlice(values_);
-
-        keys_ = newKeys;
-        values_ = newValues;
-    }
-
-    void remove(K key)
-    {
-        foreach(i, ref k; keys_)
-        {
-            if (!k.isNull && k == key)
-            {
-                k.nullify;
-                values_[i] = V.init;
-                return;
-            }
-        }
-    }
-
-    bool haveAValue() {
-        foreach(i, ref k; keys_)
-        {
-            if (!k.isNull)
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-}
