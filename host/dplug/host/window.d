b4c6a4c87e76c812e04a1ee35ab7744e8cb29214
Guillaume Piolat <contact@auburnsounds.com>
2018-01-03 15:22:33 +0100

    stress-plugin now have a IWindowListener, so that we can assume the presence of that listener.

--- host/dplug/host/window.d
+++ host/dplug/host/window.d
@@ -7,7 +7,10 @@
  */
 module dplug.host.window;
 
+import gfm.math.box;
 import dplug.window;
+import dplug.graphics.image;
+import dplug.graphics.color;
 import dplug.host.host;
 
 /// Creates a new native window suitable to host the plugin window.
@@ -16,9 +19,77 @@ IWindow createHostWindow(IPluginHost pluginHost)
 {    
     int[2] windowSize = pluginHost.getUISize();
 
-    // MAYDO: have a NullWindowListener there
-    auto hostWindow = createWindow(WindowUsage.host, null, null, null, WindowBackend.autodetect, windowSize[0], windowSize[1]);
+    auto hostWindow = createWindow(WindowUsage.host, null, null,
+                                   new NullWindowListener, WindowBackend.autodetect, windowSize[0], windowSize[1]);
     pluginHost.openUI(hostWindow.systemHandle());
 
     return hostWindow;
+}
+
+/// A listener to ignore window events, since we are not interested in host windows events.
+class NullWindowListener : IWindowListener
+{
+nothrow:
+@nogc:
+
+    bool onMouseClick(int x, int y, MouseButton mb, bool isDoubleClick, MouseState mstate)
+    {
+        return false;
+    }
+
+    bool onMouseRelease(int x, int y, MouseButton mb, MouseState mstate)
+    {
+        return false;
+    }
+
+    bool onMouseWheel(int x, int y, int wheelDeltaX, int wheelDeltaY, MouseState mstate)
+    {
+        return false;
+    }
+
+    void onMouseMove(int x, int y, int dx, int dy, MouseState mstate)
+    {
+    }
+
+    bool onKeyDown(Key key)
+    {
+        return false;
+    }
+
+    bool onKeyUp(Key key)
+    {
+        return false;
+    }
+
+    void onDraw(WindowPixelFormat pf)
+    {
+    }
+
+    ImageRef!RGBA onResized(int width, int height)
+    {
+        ImageRef!RGBA fake;
+        fake.w = width;
+        fake.h = height;
+        fake.pixels = null;
+        fake.pitch = width*RGBA.sizeof;
+        return fake;
+    }
+
+    void recomputeDirtyAreas()
+    {
+    }
+
+    box2i getDirtyRectangle()
+    { 
+        // return empty box since the host window itself is nothing interesting to draw
+        return box2i(0, 0, 0, 0); 
+    }
+
+    void onMouseCaptureCancelled()
+    {
+    }
+
+    void onAnimate(double dt, double time)
+    {
+    }
 }
