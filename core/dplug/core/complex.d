b5fbf4c801a93b48e54a7576000edad81fb29947
Guillaume Piolat <contact@auburnsounds.com>
2018-02-14 16:38:14 +0100

    More complex operations.

--- core/dplug/core/complex.d
+++ core/dplug/core/complex.d
@@ -32,16 +32,49 @@ template BuiltinImaginary(T)
         static assert("This type doesn't match any builtin imaginary complex type");
 }
 
+@safe pure nothrow @nogc:
 
 /// Returns: The argument (or phase) of `z`.
-T arg(T)(BuiltinComplex!T z) @safe pure nothrow @nogc
+float arg(cfloat z)
 {
     import std.math : atan2;
     return atan2(z.im, z.re);
 }
 
+/// Returns: The argument (or phase) of `z`.
+double arg(cdouble z)
+{
+    import std.math : atan2;
+    return atan2(z.im, z.re);
+}
 
-BuiltinComplex!T makeComplex(T)(T re, T im)
+deprecated("Use the builtin complex syntax x + y * 1i instead") BuiltinComplex!T makeComplex(T)(T re, T im)
 {
     return re + im * 1i;
+}
+
+/// Returns: The squared modulus of `z`.
+float sqAbs(cfloat z)
+{
+    return z.re*z.re + z.im*z.im;
+}
+
+/// Returns: The squared modulus of `z`.
+float sqAbs(cdouble z)
+{
+    return z.re*z.re + z.im*z.im;
+}
+
+/// Returns: Complex number from polar coordinates.
+cfloat fromPolar(float modulus, float argument)
+{
+    import std.math : sin, cos;
+    return (modulus*cos(argument)) + 1i * (modulus*sin(argument));
+}
+
+/// Returns: Complex number from polar coordinates.
+cdouble fromPolar(double modulus, double argument)
+{
+    import std.math : sin, cos;
+    return (modulus*cos(argument)) + 1i * (modulus*sin(argument));
 }
