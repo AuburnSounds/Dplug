1243c68b7c86cb582652fe454f2deeaf85b6d6df
0xEAB <desisma@heidel.beer>
2018-09-13 23:49:06 +0200

    Add "Poly Alias" synth example

---
+++ examples/poly-alias-synth/polyalias.d
@@ -0,0 +1,107 @@
+import std.math;
+import dplug.core, dplug.client, dplug.vst;
+import synthesis;
+
+// This create the DLL entry point
+mixin(DLLEntryPoint!());
+
+// This create the VST entry point
+mixin(VSTEntryPoint!PolyAlias);
+
+private
+{
+    // Number of max notes playing at the same time
+    enum maxVoices = 4;
+
+    enum Params : int
+    {
+        osc1WaveForm,
+    }
+
+    immutable waveFormNames = [__traits(allMembers, WaveForm)];
+}
+
+/// Polyphonic digital-aliasing synth
+final class PolyAlias : dplug.client.Client
+{
+nothrow @nogc:
+
+    private
+    {
+        Synth!maxVoices _synth;
+    }
+
+    public this()
+    {
+        super();
+
+        assumeNothrowNoGC((PolyAlias this_) {
+            this_._synth = mallocNew!(typeof(this._synth))(WaveForm.saw);
+        })(this);
+    }
+
+    public override
+    {
+        PluginInfo buildPluginInfo()
+        {
+            // Plugin info is parsed from plugin.json here at compile time.
+            // Indeed it is strongly recommended that you do not fill PluginInfo
+            // manually, else the information could diverge.
+            static immutable PluginInfo pluginInfo = parsePluginInfo(import("plugin.json"));
+            return pluginInfo;
+        }
+
+        override Parameter[] buildParameters()
+        {
+            auto params = makeVec!Parameter();
+            params.pushBack(mallocNew!EnumParameter(Params.osc1WaveForm, "Osc 1: Waveform", waveFormNames, 0));
+            return params.releaseData();
+        }
+
+        override LegalIO[] buildLegalIO()
+        {
+            auto io = makeVec!LegalIO();
+            io.pushBack(LegalIO(0, 1));
+            io.pushBack(LegalIO(0, 2));
+            return io.releaseData();
+        }
+
+        override int maxFramesInProcess() pure const
+        {
+            return 32; // samples only processed by a maximum of 32 samples
+        }
+
+        override void reset(double sampleRate, int maxFrames, int numInputs, int numOutputs)
+        {
+            this._synth.reset(sampleRate);
+        }
+
+        override void processAudio(const(float*)[] inputs, float*[] outputs, int frames, TimeInfo info)
+        {
+            foreach (msg; getNextMidiMessages(frames))
+            {
+                if (msg.isNoteOn())
+                {
+                    this._synth.markNoteOn(msg.noteNumber());
+                }
+                else if (msg.isNoteOff())
+                {
+                    this._synth.markNoteOff(msg.noteNumber());
+                }
+            }
+
+            this._synth.waveForm = cast(WaveForm)(readEnumParamValue(Params.osc1WaveForm));
+
+            foreach (ref sample; outputs[0][0 .. frames])
+            {
+                sample = this._synth.synthesizeNext();
+            }
+
+            // Copy output to every channel
+            foreach (chan; 1 .. outputs.length)
+            {
+                outputs[chan][0 .. frames] = outputs[0][0 .. frames];
+            }
+        }
+    }
+}
---
+++ examples/poly-alias-synth/synthesis.d
@@ -0,0 +1,308 @@
+module synthesis;
+
+import std.math;
+import dplug.core.nogc;
+import dplug.core.math;
+
+enum TAU = 2 * PI;
+
+enum WaveForm
+{
+    saw,
+    sine,
+    square,
+}
+
+final class Oscillator
+{
+@safe pure nothrow @nogc:
+
+    private
+    {
+        float _frequency;
+        float _phase = 0;
+        float _phaseSummand;
+        float _sampleRate;
+        WaveForm _waveForm;
+    }
+
+    public
+    {
+        @property
+        {
+            void frequency(float value)
+            {
+                this._frequency = value;
+                this.updatePhaseSummand();
+            }
+        }
+
+        @property
+        {
+            void sampleRate(float value)
+            {
+                this._sampleRate = value;
+                this.updatePhaseSummand();
+            }
+        }
+
+        @property
+        {
+            WaveForm waveForm()
+            {
+                return this._waveForm;
+            }
+
+            void waveForm(WaveForm value)
+            {
+                this._waveForm = value;
+            }
+        }
+    }
+
+    public this(WaveForm waveForm)
+    {
+        this._waveForm = waveForm;
+        this.updatePhaseSummand();
+    }
+
+    public
+    {
+        float synthesizeNext()
+        {
+            float sample = void;
+
+            final switch (this._waveForm) with (WaveForm)
+            {
+            case saw:
+                sample = 1.0 - (this._phase / PI);
+                break;
+
+            case sine:
+                sample = sin(this._phase);
+                break;
+
+            case square:
+                sample = (this._phase <= PI) ? 1.0 : -1.0;
+                break;
+            }
+
+            this._phase += this._phaseSummand;
+            while (this._phase >= TAU)
+            {
+                this._phase -= TAU;
+            }
+
+            return sample;
+        }
+    }
+
+    private
+    {
+        void updatePhaseSummand()
+        {
+            pragma(inline, true);
+            this._phaseSummand = (this._frequency * TAU / this._sampleRate);
+            //this._phase = 0;
+        }
+    }
+}
+
+final class VoiceStatus
+{
+@safe nothrow @nogc:
+
+    private
+    {
+        Oscillator _osc;
+        bool _isPlaying;
+        int _note;
+    }
+
+    public pure
+    {
+        @property
+        {
+            bool isPlaying()
+            {
+                return this._isPlaying;
+            }
+        }
+
+        @property
+        {
+            int note()
+            {
+                return this._note;
+            }
+        }
+
+        @property
+        {
+            void waveForm(WaveForm value)
+            {
+                this._osc.waveForm = value;
+            }
+
+            WaveForm waveForm()
+            {
+                return this._osc.waveForm;
+            }
+        }
+    }
+
+    public this(WaveForm waveForm) @system
+    {
+        this._osc = mallocNew!Oscillator(waveForm);
+        this._note = -1;
+    }
+
+    public pure
+    {
+        void play(int note) @trusted
+        {
+            this._note = note;
+            this._osc.frequency = convertMIDINoteToFrequency(note);
+            this._isPlaying = true;
+        }
+
+        void release()
+        {
+            this._isPlaying = false;
+        }
+
+        void reset(float sampleRate)
+        {
+            this.release();
+            this._osc.sampleRate = sampleRate;
+        }
+
+        float synthesizeNext()
+        {
+            if (!this._isPlaying)
+            {
+                return 0;
+            }
+
+            return this._osc.synthesizeNext();
+        }
+    }
+}
+
+final class Synth(size_t voicesCount)
+{
+    static assert(voicesCount > 0, "A synth must have at least 1 voice.");
+
+@safe nothrow:
+
+    private
+    {
+        VoiceStatus[voicesCount] _voices;
+    }
+
+    public pure @nogc
+    {
+        @property bool isPlaying()
+        {
+            foreach(v; this._voices)
+            {
+                if (v.isPlaying())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @property
+        {
+            WaveForm waveForm()
+            {
+                return this._voices[0].waveForm;
+            }
+
+            void waveForm(WaveForm value)
+            {
+                foreach (v; this._voices)
+                {
+                    v.waveForm = value;
+                }
+            }
+        }
+    }
+
+    public this(WaveForm waveForm) @system
+    {
+        foreach (ref v; this._voices)
+        {
+            v = mallocNew!VoiceStatus(waveForm);
+        }
+    }
+
+    public pure @nogc
+    {
+        void markNoteOn(int note)
+        {
+            VoiceStatus v = this.getUnusedVoice();
+            if (v is null)
+            {
+                /+
+                    No voice available
+
+                    well, one could override one, but:
+                     - always overriding the 1st one is lame
+                     - a smart algorithm would make this example more complicated
+                +/
+                return;
+            }
+
+            v.play(note);
+        }
+
+        void markNoteOff(int note)
+        {
+            foreach (v; this._voices)
+            {
+                if (v.isPlaying && (v.note == note))
+                {
+                    v.release();
+                }
+            }
+        }
+
+        void reset(float sampleRate)
+        {
+            foreach (v; this._voices)
+            {
+                v.reset(sampleRate);
+            }
+        }
+
+        float synthesizeNext()
+        {
+            float sample = 0;
+
+            foreach (v; this._voices)
+            {
+                sample += (v.synthesizeNext() / voicesCount); // synth + lower volume
+            }
+
+            return sample;
+        }
+    }
+
+    private
+    {
+        VoiceStatus getUnusedVoice()
+        {
+            foreach (v; this._voices)
+            {
+                if (!v.isPlaying)
+                {
+                    return v;
+                }
+            }
+            return null;
+        }
+    }
+}
