994256115772de4be6870b34a66b91285af0742b
Guillaume Piolat <contact@auburnsounds.com>
2018-09-16 22:10:18 +0200

    >>> Runtime Sections Episode 34 >>>
    Using a callable voldemort instead of faking delegates.

--- core/dplug/core/runtime.d
+++ core/dplug/core/runtime.d
@@ -9,6 +9,7 @@ module dplug.core.runtime;
 
 import core.stdc.stdlib;
 import std.traits;
+import std.functional: toDelegate;
 
 import dplug.core.fpcontrol;
 import dplug.core.nogc;
@@ -51,70 +52,82 @@ private:
     debug bool _entered = false;
 }
 
-template RuntimeSectionReturnType(T)
-{
-    alias RuntimeSectionReturnType = SetFunctionAttributes!(T, functionLinkage!T, functionAttributes!T | FunctionAttribute.nogc);
-}
+
 
 /// This encloses the runtime initialization and finalization.
-/// Returns: a delegate which enables the runtime and call the enclosed delegate.
-RuntimeSectionReturnType!T runtimeSection(T)(T functionOrDelegateThatCanBeGC) nothrow @nogc
+/// Returns: a callback Voldement inside which you can use the runtime, but you can't escape GC memory.
+auto runtimeSection(F)(F functionOrDelegateThatCanBeGC) nothrow @nogc if (isCallable!(F))
 {
-    static if (isDelegate!T)
+    // turn that into a delegate for simplicity purposes
+    auto myGCDelegate = toDelegate(functionOrDelegateThatCanBeGC);
+    alias T = typeof(myGCDelegate);
+
+    enum attrs = functionAttributes!T | FunctionAttribute.nogc;        
+
+    static ReturnType!T internalFunc(T fun, Parameters!T params) nothrow
     {
-        enum attrs = functionAttributes!T | FunctionAttribute.nogc;        
-
-        static ReturnType!T internalFunc(T fun, Parameters!T params) nothrow
+        try
         {
-            try
-            {
-                import core.stdc.stdio;
-                ScopedRuntimeSection section;
-                section.enter();
-                return fun(params);
+            import core.stdc.stdio;
+            ScopedRuntimeSection section;
+            section.enter();
+            return fun(params);
 
-                // Leaving runtime here
-                // all GC objects will get collected, no reference may escape safely
-            }
-            catch(Exception e)
-            {
-                // runtime initialization failed
-                // this should never happen
-                assert(false);
-            }            
-        }      
-
-        static ReturnType!T internalDg(SuperContext* thisPointer, Parameters!T params) nothrow @nogc
+            // Leaving runtime here
+            // all GC objects will get collected, no reference may escape safely
+        }
+        catch(Exception e)
         {
-            T dg;
-            dg.funcptr = cast(typeof(dg.funcptr)) (thisPointer.funPointer);
-            dg.ptr = thisPointer.outerContext;
-            return assumeNoGC(&internalFunc)(dg, params);
+            // runtime initialization failed
+            // this should never happen
+            assert(false);
+        }            
+    }      
+
+    static struct SuperContext
+    {
+        void* outerContext;
+        void* funPointer;            
+    }
+
+    static ReturnType!T internalDg(SuperContext* thisPointer, Parameters!T params) nothrow @nogc
+    {
+        T dg;
+        dg.funcptr = cast(typeof(dg.funcptr)) (thisPointer.funPointer);
+        dg.ptr = thisPointer.outerContext;
+        return assumeNoGC(&internalFunc)(dg, params);
+    }
+
+    // We return this callable Voldemort type, this allow guaranteed clean-up
+
+    static struct ManualDelegate
+    {
+        SuperContext* ptr;
+        void* funcPtr;
+
+        ReturnType!T opCall(Parameters!T params) nothrow @nogc
+        {
+            return internalDg(ptr, params);
         }
 
-        ManualDelegate fakeDg;
-        fakeDg.ptr = cast(SuperContext*) malloc( SuperContext.sizeof );
-        fakeDg.ptr.funPointer = functionOrDelegateThatCanBeGC.funcptr;
-        fakeDg.ptr.outerContext = functionOrDelegateThatCanBeGC.ptr;
-        fakeDg.funcPtr = &internalDg; // static function so no closures
+        @disable this(this);
 
-        // return this ABI-compatible `delegate` which will be used normally, but comes with a manual lifetime
-        RuntimeSectionReturnType!T* result = cast(RuntimeSectionReturnType!T*)(&fakeDg);
-        return *result;
+        ~this()
+        {
+            if (ptr)
+            {
+                free(ptr);
+                ptr = null;
+            }
+        }
     }
-    else
-        static assert(false, "must be a delegate");
-}
 
-void finalizeRuntimeSection(T)(T x)
-{
-    static if (isDelegate!T)
-    {
-        // free the context allocated in `runtimeSection`
-        free(x.ptr);
-    }
-    else
-        static assert(false, "must be a delegate");
+    ManualDelegate fakeDg;
+    fakeDg.ptr = cast(SuperContext*) malloc( SuperContext.sizeof );
+    fakeDg.ptr.funPointer = myGCDelegate.funcptr;
+    fakeDg.ptr.outerContext = myGCDelegate.ptr;
+    fakeDg.funcPtr = &internalDg; // static function so no closures
+    return fakeDg;
 }
 
 /// RAII struct for runtime initialization, to be used once by the plug-in client.
@@ -170,17 +183,7 @@ private:
 
 private:
 
-static struct SuperContext
-{
-    void* outerContext;
-    void* funPointer;            
-}
 
-static struct ManualDelegate
-{
-    SuperContext* ptr;
-    void* funcPtr;
-}
 
 
 /// RAII struct to ensure thread attacment is initialized and usable
@@ -195,6 +198,7 @@ public:
     /// Thread that shouldn't be attached are eg. the audio threads.
     void enter()
     {
+        // shoud allow reentrant threads
         bool alreadyAttached = isThisThreadAttached();
         if (!alreadyAttached)
         {
--- tests/using-runtime/main.d
+++ tests/using-runtime/main.d
@@ -40,7 +40,8 @@ nothrow:
 
     override void reset(double sampleRate, int maxFrames, int numInputs, int numOutputs) 
     {
-        // Note: this doesn't need to be `@nogc`
+        // Note: this doesn't need to be `@nogc`.
+        // This has to be a delegate, not a raw function pointer.
         int functionThatUseRuntime() nothrow 
         {
             // Note: here you can call runtime functions and stuff, however
@@ -56,7 +57,7 @@ nothrow:
             return 1984;
         }
 
-        // Note: this (FUTURE) returns a Voldemort, that require special clean-up
+        // Note: this (FUTURE) returns a callable Voldemort
         auto runtimeUsingFunction = runtimeSection(&functionThatUseRuntime); 
 
         foreach(times; 0..10)
@@ -64,7 +65,6 @@ nothrow:
             int result = runtimeUsingFunction();
             assert(result == 1984);
         }
-        finalizeRuntimeSection(runtimeUsingFunction); // this free the manual delegate context
     }
 
     override void processAudio(const(float*)[] inputs, float*[]outputs, int frames, TimeInfo info)
