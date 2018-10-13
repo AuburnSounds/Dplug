efd374bfbab8c16086e297f0c0163c5fe1d11b78
Guillaume Piolat <contact@auburnsounds.com>
2018-10-13 15:47:08 +0200

    No mipmap work to do => no CPU used. Previously we would trigger mipmap threads only to have them realize there is nothing to do, which is loosing 0.03ms every time.

--- gui/dplug/gui/graphics.d
+++ gui/dplug/gui/graphics.d
@@ -694,6 +694,10 @@ protected:
     {
         int numAreas = cast(int)_rectsToUpdateDisjointedPBR.length;
 
+        // No mipmap part to update, skip work
+        if (numAreas == 0)
+            return;
+
         // Fill update rect buffer with the content of _rectsToUpdateDisjointedPBR
         for (int i = 0; i < 2; ++i)
         {
@@ -860,7 +864,7 @@ void shuffleComponentsRGBA8ToARGB8(ImageRef!RGBA image) pure nothrow @nogc
                 _mm_storeu_si128(cast(__m128i*)(&scan[4*i]), outputBytes);
             }
             else
-            {
+            {            
                 // convert to ushort
                 __m128i zero = _mm_setzero_si128();
                 __m128i e0_7 = _mm_unpacklo_epi8(inputBytes, zero);
@@ -871,7 +875,7 @@ void shuffleComponentsRGBA8ToARGB8(ImageRef!RGBA image) pure nothrow @nogc
                 e8_15 = _mm_shufflelo_epi16!swapRB(_mm_shufflehi_epi16!swapRB(e8_15));
                 __m128i outputBytes = _mm_packus_epi16(e0_7, e8_15);
                 _mm_storeu_si128(cast(__m128i*)(&scan[4*i]), outputBytes);
-            }
+            }            
         }
 
         for(; i < w; i ++)
