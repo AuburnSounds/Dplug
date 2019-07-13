7a959bf90e97f5e56637a1f218f2a709dc6901b9
Guillaume Piolat <contact@auburnsounds.com>
2019-07-13 16:29:21 +0200

    Delayline now has duplicated write pointers, in order to make any delayline read always contiguous. This speed-up reading taps since the same pointer can be accessed all the time. Panagement 2 gets 6.5% faster thanks to this.
    For all purposes, the new gets about -120 dB abs difference with the old => very close to being identical.
    API hasn't changed, so this is marked as non-breaking.
    Sound got enhanced because index are decomposed to fractional part in double precision, which improve things on very long dealy line (~20000 samples and above).

--- dsp/dplug/dsp/delayline.d
+++ dsp/dplug/dsp/delayline.d
@@ -12,17 +12,25 @@ import dplug.core.math;
 import dplug.core.vec;
 
 /// Allow to sample signal back in time.
+/// This delay-line has a twin write index, so that the read pointer 
+/// can read a contiguous memory area.
+/// ____________________________________________________________________________________
+/// |     | _index |                  | readPointer = _index + half size |             |
+/// ------------------------------------------------------------------------------------
+///
 struct Delayline(T)
 {
 public:
+nothrow:
+@nogc:
 
     /// Initialize the delay line. Can delay up to count samples.
-    void initialize(int numSamples) nothrow @nogc
+    void initialize(int numSamples)
     {
         resize(numSamples);
     }
 
-    ~this() nothrow @nogc
+    ~this()
     {
         _data.reallocBuffer(0);
     }
@@ -31,7 +39,7 @@ public:
 
     /// Resize the delay line. Can delay up to count samples.
     /// The state is cleared.
-    void resize(int numSamples) nothrow @nogc
+    void resize(int numSamples)
     {
         if (numSamples < 0)
             assert(false);
@@ -40,16 +48,18 @@ public:
         // This wastes memory but allows delay-line of length 0 without tests.
 
         int toAllocate = nextPow2HigherOrEqual(numSamples + 1);
-        _data.reallocBuffer(toAllocate);
+        _data.reallocBuffer(toAllocate * 2);
+        _half = toAllocate;
         _indexMask = toAllocate - 1;
         _numSamples = numSamples;
         _index = _indexMask;
+
         _data[] = 0;
     }
 
     /// Combined feed + sampleFull.
     /// Uses the delay line as a fixed delay of count samples.
-    T nextSample(T incoming) nothrow @nogc
+    T nextSample(T incoming) pure
     {
         feedSample(incoming);
         return sampleFull(_numSamples);
@@ -60,21 +70,22 @@ public:
     ///
     /// Note: input and output may overlap. 
     ///       If this was ever optimized, this should preserve that property.
-    void nextBuffer(const(T)* input, T* output, int frames) nothrow @nogc
+    void nextBuffer(const(T)* input, T* output, int frames) pure
     {
         for(int i = 0; i < frames; ++i)
             output[i] = nextSample(input[i]);
     }
 
     /// Adds a new sample at end of delay.
-    void feedSample(T incoming) nothrow @nogc
+    void feedSample(T incoming) pure
     {
         _index = (_index + 1) & _indexMask;
-        _data[_index] = incoming;
+        _data.ptr[_index] = incoming;
+        _data.ptr[_index + _half] = incoming;
     }
 
     /// Adds several samples at end of delay.
-    void feedBuffer(const(T)[] incoming) nothrow @nogc
+    void feedBuffer(const(T)[] incoming) pure
     {
         int N = cast(int)(incoming.length);
 
@@ -88,73 +99,81 @@ public:
         if (N <= remain)
         {
             memcpy( &_data[_index + 1], incoming.ptr, N * T.sizeof );
+            memcpy( &_data[_index + 1 + _half], incoming.ptr, N * T.sizeof );
             _index += N;
         }
         else
         {
             memcpy( _data.ptr + (_index + 1), incoming.ptr, remain * T.sizeof );
+            memcpy( _data.ptr + (_index + 1) + _half, incoming.ptr, remain * T.sizeof );
             size_t numBytes = (N - remain) * T.sizeof;
             memcpy( _data.ptr, incoming.ptr + remain, numBytes);
+            memcpy( _data.ptr + _half, incoming.ptr + remain, numBytes);
             _index = (_index + N) & _indexMask;
         }
     }
 
+    /// Returns: A pointer which allow to get delayed values.
+    ///    readPointer()[0] is the last samples fed,  readPointer()[-1] is the penultimate.
+    /// Warning: it goes backwards, increasing delay => decreasing addressed.
+    const(T)* readPointer() pure const
+    {
+        return _data.ptr + _index + _half;
+    }
+
     /// Random access sampling of the delay-line at integer points.
     /// Delay 0 = last entered sample with feed().
-    T sampleFull(int delay) nothrow @nogc
+    T sampleFull(int delay) pure
     {
         assert(delay >= 0);
-        return _data[(_index - delay) & _indexMask];
+        return readPointer()[-delay];
     }
 
     static if (is(T == float) || is(T == double))
     {
         /// Random access sampling of the delay-line with linear interpolation.
-        T sampleLinear(float delay) nothrow @nogc
+        T sampleLinear(float delay) pure const
         {
             assert(delay > 0);
-            float sampleLoc = (_index - delay) + 2 * _data.length;
-            assert(sampleLoc >= 1);
-            int iPart = cast(int)(sampleLoc);
-            float fPart = cast(float)(sampleLoc - iPart);
-            T x0  = _data[iPart       & _indexMask];
-            T x1  = _data[(iPart + 1) & _indexMask];
+            int iPart;
+            float fPart;
+            decomposeFractionalDelay(delay, iPart, fPart);
+            const(T)* pData = readPointer();
+            T x0  = pData[iPart];
+            T x1  = pData[iPart + 1];
             return lerp(x0, x1, fPart);
         }
 
         /// Random access sampling of the delay-line with a 3rd order polynomial.
-        T sampleHermite(float delay) nothrow @nogc
+        T sampleHermite(float delay) pure const
         {
             assert(delay > 1);
-            float sampleLoc = (_index - delay) + 2 * _data.length;
-            assert(sampleLoc >= 1);
-            int iPart = cast(int)(sampleLoc);
-            float fPart = cast(float)(sampleLoc - iPart);
-            assert(fPart >= 0.0f);
-            assert(fPart <= 1.0f);
-            T xm1 = _data[(iPart - 1) & _indexMask];
-            T x0  = _data[ iPart      & _indexMask];
-            T x1  = _data[(iPart + 1) & _indexMask];
-            T x2  = _data[(iPart + 2) & _indexMask];
+            int iPart;
+            float fPart;
+            decomposeFractionalDelay(delay, iPart, fPart);
+            const(T)* pData = readPointer();
+            T xm1 = pData[iPart-1];
+            T x0  = pData[iPart  ];
+            T x1  = pData[iPart+1];
+            T x2  = pData[iPart+2];
             return hermite!T(fPart, xm1, x0, x1, x2);
         }
 
-        /// Third-order splice interpolation
+        /// Third-order spline interpolation
         /// http://musicdsp.org/showArchiveComment.php?ArchiveID=62
-        T sampleSpline3(float delay)
+        T sampleSpline3(float delay) pure const
         {
             assert(delay > 1);
-            float sampleLoc = (_index - delay) + 2 * _data.length;
-            assert(sampleLoc >= 1);
-
-            int iPart = cast(int)(sampleLoc);
-            float fPart = cast(float)(sampleLoc - iPart);
+            int iPart;
+            float fPart;
+            decomposeFractionalDelay(delay, iPart, fPart);
             assert(fPart >= 0.0f);
             assert(fPart <= 1.0f);
-            T L1 = _data[(iPart - 1) & _indexMask];
-            T L0  = _data[ iPart      & _indexMask];
-            T H0  = _data[(iPart + 1) & _indexMask];
-            T H1  = _data[(iPart + 2) & _indexMask];
+            const(T)* pData = readPointer();
+            T L1 = pData[iPart-1];
+            T L0  = pData[iPart  ];
+            T H0  = pData[iPart+1];
+            T H1  = pData[iPart+2];
 
             return L0 + 0.5f *
                 fPart*(H0-L1 +
@@ -166,26 +185,20 @@ public:
 
         /// 4th order spline interpolation
         /// http://musicdsp.org/showArchiveComment.php?ArchiveID=60
-        T sampleSpline4(float delay)
+        T sampleSpline4(float delay) pure const
         {
             assert(delay > 2);
-            T sampleLoc = (_index - delay) + 2 * _data.length;
-            assert(sampleLoc >= 2);
-
-            int iPart = cast(int)(sampleLoc);
-            T fPart = cast(T)(sampleLoc - iPart);
-
-
-            assert(fPart >= 0.0f);
-            assert(fPart <= 1.0f);
-
-            T p0 = _data[(iPart-2) & _indexMask];
-            T p1 = _data[(iPart-1) & _indexMask];
-            T p2 = _data[iPart     & _indexMask];
-            T p3 = _data[(iPart+1) & _indexMask];
-            T p4 = _data[(iPart+2) & _indexMask];
-            T p5 = _data[(iPart+3) & _indexMask];
-
+            int iPart;
+            float fPart;
+            decomposeFractionalDelay(delay, iPart, fPart);
+            const(T)* pData = readPointer();
+            T p0 = pData[iPart-2];
+            T p1 = pData[iPart-1];
+            T p2 = pData[iPart  ];
+            T p3 = pData[iPart+1];
+            T p4 = pData[iPart+2];
+            T p5 = pData[iPart+3];
+            
             return p2 + 0.04166666666f * fPart * ((p3 - p1) * 16 + (p0 - p4) * 2
             + fPart * ((p3 + p1) * 16 - p0 - p2 * 30 - p4
             + fPart * (p3 * 66 - p2 * 70 - p4 * 33 + p1 * 39 + p5 * 7- p0 * 9
@@ -197,8 +210,26 @@ public:
 private:
     T[] _data;
     int _index;
+    int _half; // half the size of the data
     int _indexMask;
     int _numSamples;
+
+    void decomposeFractionalDelay(float delay, 
+                                  out int outIntegerPart, 
+                                  out float outFloatPart) pure const
+    {
+        // Because float index can yield suprising low precision with interpolation  
+        // So we up the precision to double in order to have a precise fractional part          
+        int offset = cast(int)(_data.length);
+        double doubleDelayMinus = cast(double)(-delay);
+        int iPart = cast(int)(doubleDelayMinus + offset);
+        iPart -= offset;
+        float fPart = cast(float)(doubleDelayMinus - iPart);
+        assert(fPart >= 0.0f);
+        assert(fPart <= 1.0f);
+        outIntegerPart = iPart;
+        outFloatPart = fPart;
+    }
 }
 
 unittest
@@ -209,9 +240,15 @@ unittest
 
     Delayline!double line2;
 
-    Delayline!int line3;
+    Delayline!float line3;
     line3.initialize(2);
     assert(line3.nextSample(1) == 0);
     assert(line3.nextSample(2) == 0);
     assert(line3.nextSample(3) == 1);
+    assert(line3.nextSample(42) == 2);
+
+    assert(line3.sampleFull(0) == 42);
+    assert(line3.sampleFull(1) == 3);
+    assert(line3.sampleLinear(0.5f) == (3.0f + 42.0f) * 0.5f);
 }
+
