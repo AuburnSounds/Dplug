4aaf669e7f2c83525c6e5f6f20214e0ae053d851
Andrey Penechko <mrsmith33@yandex.ru>
2019-10-20 23:19:01 +0300

    Implement stack allocator.

---
+++ core/dplug/core/stackallocator.d
@@ -0,0 +1,104 @@
+/**
+Stack allocator for temporary allocations.
+
+Copyright: Auburn Sounds 2019.
+License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+*/
+module dplug.core.stackallocator;
+
+import core.stdc.stdlib: malloc;
+import dplug.core.vec;
+
+struct StackAllocator
+{
+private:
+    Vec!(ubyte*) bucketArray;
+    uint numUsedPages;
+    uint currentPageFreeBytes;
+    enum PAGE_SIZE = 1024 * 1024 * 1024; // 1MB
+
+public:
+
+    /// Save allocation state
+    StackAllocState saveState()
+    {
+        return StackAllocState(numUsedPages, currentPageFreeBytes);
+    }
+
+    /// Pop allocation state
+    void restoreState(StackAllocState state)
+    {
+        numUsedPages = state.savedNumUsedPages;
+        currentPageFreeBytes = state.savedCurrentPageFreeBytes;
+    }
+
+    /// return pointer to len x T.sizeof bytes of uninitialized memory
+    T[] makeArray(T)(size_t len)
+    {
+        size_t allocSize = len * T.sizeof;
+        assert(allocSize <= PAGE_SIZE, "Requested size is bigger that page size");
+
+        if (currentPageFreeBytes < allocSize)
+            setupNextPage;
+
+        size_t nextByte = PAGE_SIZE - currentPageFreeBytes;
+        currentPageFreeBytes -= allocSize;
+
+        ubyte* pagePtr = bucketArray[numUsedPages-1];
+        ubyte[] bytes = pagePtr[nextByte..nextByte+allocSize];
+
+        return cast(T[])bytes;
+    }
+
+    private void setupNextPage()
+    {
+        if (numUsedPages == bucketArray.length)
+        {
+            ubyte* newBucket = cast(ubyte*)malloc(PAGE_SIZE);
+            bucketArray.pushBack(newBucket);
+        }
+        // alloc from new page
+        ++numUsedPages;
+        currentPageFreeBytes = PAGE_SIZE;
+    }
+}
+
+struct StackAllocState
+{
+    private uint savedNumUsedPages;
+    private uint savedCurrentPageFreeBytes;
+}
+
+unittest
+{
+    StackAllocator allocator;
+    auto saved = allocator.saveState;
+    uint[] arr = allocator.makeArray!uint(10);
+    arr[] = 42;
+    assert(arr.length == 10);
+    allocator.restoreState(saved);
+
+    uint[] arr2 = allocator.makeArray!uint(10);
+    arr2[] = 48;
+
+    assert(arr[0] == 48);
+
+    // multiple allocations
+    uint[] arr3 = allocator.makeArray!uint(10);
+    arr3[] = 60;
+
+    // doesn't overwrite arr2
+    assert(arr2[0] == 48);
+    assert(arr2[$-1] == 48);
+
+    allocator.restoreState(saved);
+
+    // test new page allocation
+    allocator.makeArray!uint(1);
+    allocator.makeArray!uint(StackAllocator.PAGE_SIZE / uint.sizeof);
+
+    allocator.restoreState(saved);
+
+    // test page reuse
+    allocator.makeArray!uint(StackAllocator.PAGE_SIZE / uint.sizeof);
+}
