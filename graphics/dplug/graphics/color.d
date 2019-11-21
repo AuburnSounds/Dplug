AuburnSounds/Dplug

commit 1cf37e6725637dccb3b7377c08a96b99ad6b773f

Optimize specular highlight, PBR itself is speed up by about 3%.

gui/dplug/gui/compositor.d
@@ -91,16 +91,22 @@ nothrow @nogc:
 
         _normalBuffer = mallocNew!(OwnedImage!RGBf)();
         _accumBuffer = mallocNew!(OwnedImage!RGBAf)();
+        float[] _specularFactor;
+        float[] _exponentFactor;
     }
 
     ~this()
     {
         _normalBuffer.destroyFree();
         _accumBuffer.destroyFree();
+        _specularFactor.reallocBuffer(0);
+        _exponentFactor.reallocBuffer(0);
     }
 
     override void resizeBuffers(int width, int height)
     {
+        _specularFactor.reallocBuffer(width);
+        _exponentFactor.reallocBuffer(width);
         _normalBuffer.size(width, height);
         _accumBuffer.size(width, height);
     }
@@ -327,6 +333,7 @@ nothrow @nogc:
 
         // Specular highlight
         {
+            __m128 mmlight3Dir = _mm_setr_ps(-light3Dir.x, -light3Dir.y, -light3Dir.z, 0.0f);
             for (int j = area.min.y; j < area.max.y; ++j)
             {
                 RGBA* materialScan = materialMap.levels[0].scanlinePtr(j);
@@ -334,33 +341,58 @@ nothrow @nogc:
                 RGBf* normalScan = _normalBuffer.scanlinePtr(j);
                 RGBAf* accumScan = _accumBuffer.scanlinePtr(j);
 
+                float* pSpecular = _specularFactor.ptr;
+                float* pExponent = _exponentFactor.ptr;
+
+
                 for (int i = area.min.x; i < area.max.x; ++i)
                 {
                     RGBA materialHere = materialScan[i];
-                    float roughness = materialHere.r * div255;
-                    float metalness = materialHere.g * div255;
-                    float specular  = materialHere.b * div255;
-                    RGBA ibaseColor = diffuseScan[i];
-                    vec3f baseColor = vec3f(ibaseColor.r, ibaseColor.g, ibaseColor.b) * div255;
-
                     RGBf normalFromBuf = normalScan[i];
-                    vec3f normal = vec3f(normalFromBuf.r, normalFromBuf.g, normalFromBuf.b);
+                    __m128 normal = convertNormalToFloat4(normalScan[i]);
 
-                    vec3f toEye = vec3f(0.5f - i * invW, j * invH - 0.5f, 1.0f);
-                    toEye.fastNormalize(); // this makes very, very little difference in output vs normalize
+                    // TODO: this should be tuned interactively, maybe it's annoying to feel
+                    __m128 toEye = _mm_setr_ps(0.5f - i * invW, j * invH - 0.5f, 1.0f, 0.0f);
 
-                    vec3f lightReflect = reflect(-light3Dir, normal);
-                    float specularFactor = dot(toEye, lightReflect);
-                    if (specularFactor > 1e-3f)
+                    toEye = _mm_fast_normalize_ps(toEye);
+                    __m128 lightReflect = _mm_reflectnormal_ps(mmlight3Dir, normal);
+                    float specularFactor = _mm_dot_ps(toEye, lightReflect);
+                    if (specularFactor < 1e-3f) 
+                        specularFactor = 1e-3f;
+                    pSpecular[i] = specularFactor;
+                    pExponent[i] = _exponentTable[materialHere.r];
+                }
+
+                // Just the pow operation for this line
+                {
+                    int i = area.min.x;
+                    for (; i + 3 < area.max.x; i += 4)
                     {
-                        float exponent = _exponentTable[materialHere.r];
-                        specularFactor = _mm_pow_ss(specularFactor, exponent); // TODO: do 4 pixels at once
-                        float roughFactor = 10 * (1.0f - roughness) * (1 - metalness * 0.5f);
-                        specularFactor = specularFactor * roughFactor;
-
-                        vec3f color = baseColor * light3Color * (specularFactor * specular);
-                        accumScan[i] += RGBAf(color.r, color.g, color.b, 0.0f);
+                        _mm_storeu_ps(&pSpecular[i], _mm_pow_ps(_mm_loadu_ps(&pSpecular[i]), _mm_loadu_ps(&pExponent[i])));
                     }
+                    for (; i < area.max.x; ++i)
+                    {
+                        pSpecular[i] = _mm_pow_ss(pSpecular[i], pExponent[i]);
+                    }
+                }
+
+                for (int i = area.min.x; i < area.max.x; ++i)
+                {
+                    float specularFactor = pSpecular[i];
+
+                    __m128 material = convertMaterialToFloat4(materialScan[i]);
+                    RGBA materialHere = materialScan[i];
+                    float roughness = material.array[0];
+                    float metalness = material.array[1];
+                    float specular  = material.array[2];
+                    __m128 baseColor = convertBaseColorToFloat4(diffuseScan[i]);
+                    __m128 mmLight3Color = _mm_setr_ps(light3Color.x, light3Color.y, light3Color.z, 0.0f);
+
+                    float roughFactor = 10 * (1.0f - roughness) * (1 - metalness * 0.5f);
+                    specularFactor = specularFactor * roughFactor;
+                    __m128 color = baseColor * mmLight3Color * _mm_set1_ps(specularFactor * specular);
+
+                    _mm_store_ps(cast(float*)(&accumScan[i]), _mm_load_ps(cast(float*)(&accumScan[i])) + color);
                 }
             }
         }
@@ -505,6 +537,10 @@ private:
     OwnedImage!RGBf _normalBuffer;
     OwnedImage!RGBAf _accumBuffer;
     float[256] _exponentTable;
+
+    // For the specular highlight pass
+    float[] _specularFactor;
+    float[] _exponentFactor;
 }
 
 private:
@@ -559,6 +595,8 @@ float fastlog2(float val) pure nothrow @nogc
     return fi.f + log_2;
 }
 
+alias convertMaterialToFloat4 = convertBaseColorToFloat4;
+
 // Convert a 8-bit color to a normalized 4xfloat color
 __m128 convertBaseColorToFloat4(RGBA rgba) nothrow @nogc pure
 {
@@ -567,8 +605,11 @@ __m128 convertBaseColorToFloat4(RGBA rgba) nothrow @nogc pure
     __m128i mmZero = _mm_setzero_si128();
     __m128i shorts = _mm_unpacklo_epi8(packed, mmZero);
     __m128i ints = _mm_unpacklo_epi16(shorts, mmZero);
-
     enum float div255 = 1 / 255.0f;
     return _mm_cvtepi32_ps(ints) * _mm_set1_ps(div255);
 }
 
+__m128 convertNormalToFloat4(RGBf normal) nothrow @nogc pure
+{
+    return _mm_setr_ps(normal.r, normal.g, normal.b, 0.0f);
+}

gui/dplug/gui/ransac.d
@@ -11,19 +11,34 @@ alias RansacMode = int;
 nothrow @nogc:
 
 
+// Note: these functions should move to intel-intrinsics #BONUS
+
+/// 4D dot product
+package float _mm_dot_ps(__m128 a, __m128 b) pure
+{
+    __m128 m = a * b;
+    return m.array[0] + m.array[1] + m.array[2] + m.array[3];
+}
 
 // Note: .w element is undefined
-__m128 _mm_crossproduct_ps(__m128 a, __m128 b) pure
+package __m128 _mm_crossproduct_ps(__m128 a, __m128 b) pure
 {
     enum ubyte SHUF1 = _MM_SHUFFLE(3, 0, 2, 1);
     enum ubyte SHUF2 = _MM_SHUFFLE(3, 1, 0, 2);
     return _mm_sub_ps(
-        _mm_mul_ps(_mm_shuffle_ps!SHUF1(a, a), _mm_shuffle_ps!SHUF2(b, b)), 
-        _mm_mul_ps(_mm_shuffle_ps!SHUF2(a, a), _mm_shuffle_ps!SHUF1(b, b))
-    );
+                      _mm_mul_ps(_mm_shuffle_ps!SHUF1(a, a), _mm_shuffle_ps!SHUF2(b, b)), 
+                      _mm_mul_ps(_mm_shuffle_ps!SHUF2(a, a), _mm_shuffle_ps!SHUF1(b, b))
+                      );
 }
 
-__m128 _mm_fast_normalize_ps(__m128 v) pure
+package __m128 _mm_reflectnormal_ps(__m128 normalA, __m128 normalB) pure // W must be zero
+{
+    __m128 dotBA = normalB * normalA;
+    float sum = 2 * _mm_dot_ps(normalA, normalB);
+    return normalA - _mm_set1_ps(sum) * normalB;
+}
+
+package __m128 _mm_fast_normalize_ps(__m128 v) pure
 {
     __m128 squared = _mm_mul_ps(v, v);
     float squaredLength = squared.array[0] + squared.array[1] + squared.array[2] + squared.array[3];

commit a4911d079c007eaa2584fc2c26018bc9997146e2

Fix race condition in compositor, introduced in 1cf37e6725637dccb3b7377c08a96b99ad6b773f Avoid v9.0.17 and v9.0.18

graphics/dplug/graphics/color.d
@@ -312,10 +312,14 @@ alias Color!(ubyte  , "r", "g", "b", "a") RGBA   ;
 
 alias Color!(ubyte  , "l"               ) L8     ;
 alias Color!(ushort , "l"               ) L16    ;
+alias Color!(float ,  "l"               ) L32f   ;
 
 alias Color!(float  , "r", "g", "b"     ) RGBf   ;
 alias Color!(float  , "r", "g", "b", "a") RGBAf  ;
 
+static assert(L32f.sizeof == 4);
+static assert(RGBf.sizeof == 12);
+static assert(RGBAf.sizeof == 16);
 
 unittest
 {

gui/dplug/gui/compositor.d
@@ -91,22 +91,23 @@ nothrow @nogc:
 
         _normalBuffer = mallocNew!(OwnedImage!RGBf)();
         _accumBuffer = mallocNew!(OwnedImage!RGBAf)();
-        float[] _specularFactor;
-        float[] _exponentFactor;
+
+        _specularFactor = mallocNew!(OwnedImage!L32f)();
+        _exponentFactor = mallocNew!(OwnedImage!L32f)();
     }
 
     ~this()
     {
         _normalBuffer.destroyFree();
         _accumBuffer.destroyFree();
-        _specularFactor.reallocBuffer(0);
-        _exponentFactor.reallocBuffer(0);
+        _specularFactor.destroyFree();
+        _exponentFactor.destroyFree();
     }
 
     override void resizeBuffers(int width, int height)
     {
-        _specularFactor.reallocBuffer(width);
-        _exponentFactor.reallocBuffer(width);
+        _specularFactor.size(width, height);
+        _exponentFactor.size(width, height);
 
         int border_0 = 0;
         int rowAlign_1 = 1;
@@ -348,8 +349,8 @@ nothrow @nogc:
                 RGBf* normalScan = _normalBuffer.scanlinePtr(j);
                 RGBAf* accumScan = _accumBuffer.scanlinePtr(j);
 
-                float* pSpecular = _specularFactor.ptr;
-                float* pExponent = _exponentFactor.ptr;
+                float* pSpecular = cast(float*) _specularFactor.scanlinePtr(j);
+                float* pExponent = cast(float*) _exponentFactor.scanlinePtr(j);
 
 
                 for (int i = area.min.x; i < area.max.x; ++i)
@@ -418,6 +419,7 @@ nothrow @nogc:
                 // First compute the needed mipmap level for this line
 
                 immutable float amountOfSkyboxPixels = skybox.width * skybox.height;
+                float* mipmapLevelScan = cast(float*) _skyMimapLevel.scanlinePtr(j);
                 for (int i = area.min.x; i < area.max.x; ++i)
                 {
                     // TODO optimize this crap
@@ -444,7 +446,7 @@ nothrow @nogc:
                     depthDX *= (1 / 256.0f);
                     depthDY *= (1 / 256.0f);
 
-                    _skyMimapLevel[i] = (depthDX * depthDX + depthDY * depthDY) * amountOfSkyboxPixels;
+                    mipmapLevelScan[i] = (depthDX * depthDX + depthDY * depthDY) * amountOfSkyboxPixels;
                 }
 
                 // Compute mipmap level
@@ -459,15 +461,15 @@ nothrow @nogc:
                         __m128i material4 = _mm_loadu_si128(cast(__m128i*) &materialScan[i]);
                         material4 = _mm_and_si128(material4, _mm_set1_epi32(255));
                         __m128 roughness = _mm_cvtepi32_ps(material4);
-                        __m128 derivativeSquared = _mm_loadu_ps(&_skyMimapLevel[i]);
+                        __m128 derivativeSquared = _mm_loadu_ps(&mipmapLevelScan[i]);
                         __m128 level = _mm_set1_ps(0.5f) * _mm_fastlog2_ps(_mm_set1_ps(1.0f) + derivativeSquared * _mm_set1_ps(0.00001f))
                                        + _mm_set1_ps(ROUGH_FACT) * roughness;
-                        _mm_storeu_ps(&_skyMimapLevel[i], level);
+                        _mm_storeu_ps(&mipmapLevelScan[i], level);
                     }
                     for (; i < area.max.x; ++i)
                     {
                         float roughness = materialScan[i].r;
-                        _skyMimapLevel[i] = 0.5f * fastlog2(1.0f + _skyMimapLevel[i] * 0.00001f) + ROUGH_FACT * roughness;
+                        mipmapLevelScan[i] = 0.5f * fastlog2(1.0f + mipmapLevelScan[i] * 0.00001f) + ROUGH_FACT * roughness;
                     }
                 }
 
@@ -489,7 +491,7 @@ nothrow @nogc:
                     __m128 baseColor = convertBaseColorToFloat4(diffuseScan[i]);
                     float skyx = 0.5f + ((0.5f - pureReflection.array[0] * 0.5f) * fskyX);
                     float skyy = 0.5f + ((0.5f + pureReflection.array[1] * 0.5f) * fSkyY);
-                    __m128 skyColorAtThisPoint = convertVec4fToFloat4( skybox.linearMipmapSample(_skyMimapLevel[i], skyx, skyy) );
+                    __m128 skyColorAtThisPoint = convertVec4fToFloat4( skybox.linearMipmapSample(mipmapLevelScan[i], skyx, skyy) );
                     __m128 color = baseColor * skyColorAtThisPoint * _mm_set1_ps(metalness * amount);
                     _mm_store_ps(cast(float*)(&accumScan[i]), _mm_load_ps(cast(float*)(&accumScan[i])) + color);
                 }
@@ -567,8 +569,10 @@ private:
     float[256] _exponentTable;
 
     // For the specular highlight pass
-    float[] _specularFactor;
-    float[] _exponentFactor;
+    // These buffers must be complete image, since multiple threads may be operating
+    // and overwriting the same are if it was a line.
+    OwnedImage!L32f _specularFactor;
+    OwnedImage!L32f _exponentFactor;
 
     // Reused temporary buffer for the mipmap level
     alias _skyMimapLevel = _specularFactor;