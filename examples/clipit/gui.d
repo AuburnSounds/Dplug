4ac080bc97481a91dfe057ddcfeb43a4c776d335
Guillaume Piolat <contact@auburnsounds.com>
2021-03-25 23:48:21 +0100

    Alternative ways of testing ClipIt.

--- examples/clipit/gui.d
+++ examples/clipit/gui.d
@@ -29,11 +29,9 @@ nothrow:
         super(500, 500); // fixed size
 
         //static immutable float[8] scales = [1.0f, 1.25f, 1.5f];
-        //auto constraints = makeSizeConstraintsDiscrete(500, 500, scales);
-        //super(constraints); // WIP
+        //super(makeSizeConstraintsDiscrete(500, 500, scales)); // WIP
 
-        int W = 500;
-        int H = 500;
+        //super(makeSizeConstraintsContinuous(500, 500, 1.0f, 3.0f)); // WIP
 
         // Sets the number of pixels recomputed around dirtied controls.
         // Since we aren't using PBR we can set this value to 0 to save
@@ -114,13 +112,13 @@ nothrow:
             gradient.addColorStop(0.0f, RGBA(255, 50, 128, 255));
             gradient.addColorStop(1.0f, RGBA(128, 128, 128, 0));
 
-            canvas.fillStyle = RGBA(255, 0, 0, 255);//gradient;
-
+         //   canvas.fillStyle = RGBA(255, 0, 0, 255);//gradient;
+/*
             canvas.beginPath;
                 canvas.moveTo(0, 0);
                 canvas.lineTo(200, 0);
                 canvas.lineTo(0, 200);
-                canvas.fill();
+                canvas.fill();*/
         }
 
     }
