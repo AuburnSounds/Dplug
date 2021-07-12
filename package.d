092e2cda8eb195dd065f7b08613235914f0fdf47
Guillaume Piolat <contact@auburnsounds.com>
2021-07-12 14:10:09 +0200

    Add top-level dpldocs DDoc.

---
+++ package.d
@@ -0,0 +1,11 @@
+/++
+    Dplug is an open-source audio plug-in framework existing since 2013.
+
+    A plugin framework is a library whose aim is to make cross-platform audio development 
+    a practical reality. As such, Dplug can be used to create audio effects (such as a VST 2.4) 
+    for use in popular host programs (such as FLStudio, Logic, Pro Tools, Cubase...).
+
+       - [dplug.org](https://dplug.org/)
+       - [The Dplug Wiki](https://github.com/AuburnSounds/Dplug/wiki)
+       - [ARCHITECTURE.md](https://github.com/AuburnSounds/Dplug/blob/master/ARCHITECTURE.md)
++/
