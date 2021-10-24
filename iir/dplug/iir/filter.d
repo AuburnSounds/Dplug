e8cba929ee1a1efcbd5ed971b4543d72ef09ca4d
Guillaume Piolat <contact@auburnsounds.com>
2021-10-24 21:05:18 +0200

    Obviously early dplug.iir... this is not at all even begin.

---
+++ iir/dplug/iir/filter.d
@@ -0,0 +1,31 @@
+/**
+Infinite impulse response (IIR) filters.
+
+Copyright: Copyright Guillaume Piolat 2021.
+License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+*/
+module dplug.iir.filter;
+
+nothrow:
+@nogc:
+@safe:
+
+/*
+struct IIRFilter
+{
+public nothrow @nogc @safe:
+
+     void initialize()
+
+
+
+     void process(AudioBuffer)
+
+
+
+
+
+private:
+
+}
+*/
---
+++ iir/dplug/iir/package.d
@@ -0,0 +1,9 @@
+/**
+Infinite impulse response (IIR) filters.
+
+Copyright: Copyright Guillaume Piolat 2021.
+License:   $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost License 1.0)
+*/
+module dplug.iir;
+
+
