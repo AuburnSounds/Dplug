197ed41e654355aeb88c632467d118985199af51
Guillaume Piolat <contact@auburnsounds.com>
2021-12-28 02:53:45 +0100

    wav-compare can output spectrograms of WAV difference.

--- tools/wav-compare/source/main.d
+++ tools/wav-compare/source/main.d
@@ -4,10 +4,12 @@ import std.math;
 import std.string;
 import std.array;
 import std.conv;
+import std.file;
 
 import waved;
 import dplug.core;
 import dplug.graphics;
+import dplug.dsp;
 import colorize;
 
 
@@ -22,6 +24,9 @@ void usage()
     writeln("Flags:");
     writeln("        -h, --help   Shows this help");
     writeln("        -o <file>    Write instantaneous peak difference in a WAV file (default: no)");
+    writeln("        -s <file>    Output spectrogram (default: no)");
+    writeln("        -sw <width>  Spectrogram width (default: length / 512)");
+    writeln("        -sh <height> Spectrogram height (default: 800)");
     writeln("        --strip <s>  Strip s seconds of the start. Avoid initialization conditions.");
     writeln("        --quiet      Less verbose output, just output RMS error (default: verbose)");    
     writeln;
@@ -36,6 +41,9 @@ int main(string[] args)
         string outDiffFile = null;
         bool quiet = false;
         double stripSecs = 0.0;
+        string spectrogramPath = null;
+        int sh = 800;
+        int sw = 0;
 
         for (int i = 1; i < args.length; ++i)
         {
@@ -55,6 +63,26 @@ int main(string[] args)
             {
                 i += 1;
                 stripSecs = to!double(args[i]);
+            }
+            else if (arg == "-sw")
+            {
+                i += 1;
+                sw = to!int(args[i]);
+            }
+            else if (arg == "-sh")
+            {
+                i += 1;
+                sh = to!int(args[i]);
+            }
+            else if (arg == "--strip")
+            {
+                i += 1;
+                stripSecs = to!double(args[i]);
+            }
+            else if (arg == "-s")
+            {
+                i += 1;
+                spectrogramPath = args[i];
             }
             else
                 files ~= arg;
@@ -92,8 +120,12 @@ int main(string[] args)
 
         int startSample = cast(int)(stripSecs * soundA.sampleRate);
 
+        if (sw == 0)
+            sw = soundB.lengthInFrames() / 512;
+
         // Normalize both sounds by the same amount
         // However we don't apply this factor yet to avoid introducing noise.
+        // TODO: harmonic square root?
         real nFactor = (normalizeFactor(soundA) + normalizeFactor(soundB) ) * 0.5f;
         int channels = soundA.channels;
 
@@ -131,15 +163,15 @@ int main(string[] args)
         {
             cwriteln;
             cwritefln(" Comparing %s vs %s", fileA.color(fg.light_white), fileB.color(fg.light_white));
-            writeln();
+            cwriteln();
             cwriteln ("    =================================".color(fg.light_white));
             cwritefln("        RMS difference = %s", format("%.2f dB", rms_dB).color(fg.light_yellow));
             cwriteln ("    =================================".color(fg.light_white));
-            writeln();
+            cwriteln();
 
             cwriteln(" An opinion from the comparison program:");
             cwriteln(format("    \"%s\"", getComment(rms_dB, peak_dB)).color(fg.light_cyan));
-            writeln();
+            cwriteln();
 
             if (isFinite(peak_dB - rms_dB))
                 cwritefln(" => (Peak - RMS) Crest difference = %s", format("%.2f dB", peak_dB - rms_dB).color(fg.light_green));
@@ -162,14 +194,20 @@ int main(string[] args)
             }
         }
 
+        if (spectrogramPath)
+        {
+            // Note: this will only show the left channel. 
+            outputSpectrumOfDifferences(soundA, soundB, spectrogramPath, sw, sh, nFactor, quiet);
+        }
+
         if (!quiet) cwriteln;
         return 0;
     }
     catch(Exception e)
     {
-        writeln;
+        cwriteln;
         cwritefln(format("error: %s", e.msg).color(fg.light_red));
-        writeln;
+        cwriteln;
         usage();
         writeln;
         return 1;
@@ -218,4 +256,112 @@ string getComment(float rms_dB, float peak_dB)
     {
         return "I wouldn't even bother trying to hear this difference. It must be incredibly subtle.";
     }
+}
+
+void outputSpectrumOfDifferences(Sound soundA, Sound soundB, string spectrogramPath, int sw, int sh, real nFactor, bool quiet)
+{
+    if (!quiet)
+    {
+        cwriteln;
+        cwritef(" Create a difference PNG in %s", spectrogramPath.color(fg.light_white));        
+    }
+
+    assert(soundA.sampleRate == soundB.sampleRate);
+    assert(soundA.lengthInFrames == soundB. lengthInFrames);
+
+    float samplerate = soundA.sampleRate;
+    int N = soundA.lengthInFrames();
+    float[] diff = new float[N];
+    float[] A = soundA.channel(0);
+    float[] B = soundB.channel(0);
+    foreach(n; 0..N)
+    {
+        diff[n] = nFactor * (cast(real)A[n] - B[n]); // so that the relative difference is seen.
+    }
+
+    // What is the analysisPeriod that let us hit sw as length?
+    int analysisPeriod = cast(int)(N / sw);
+    if (analysisPeriod < 1) analysisPeriod = 1;
+
+    // Good window size for this?
+    int windowSize = (analysisPeriod * 2);
+
+    // window size cannot be inferior to 20ms
+    int minWindowSize = cast(int)(0.5f + samplerate * 0.020);
+    if (windowSize < minWindowSize)
+        windowSize = minWindowSize;
+
+    int fftSize = nextPow2HigherOrEqual(windowSize);
+    bool zeroPhaseWindowing = false;
+    FFTAnalyzer!float fft;
+    fft.initialize(windowSize, fftSize, analysisPeriod, WindowDesc(WindowType.hann, WindowAlignment.right), zeroPhaseWindowing);
+
+    int half = fftSize/2 + 1;
+
+    // Create an intermediate image
+    int iheight = half;
+    int iwidth = (N + analysisPeriod - 1) / analysisPeriod; // max number of analysis
+
+    OwnedImage!RGBA temp = new OwnedImage!RGBA(iwidth, iheight);
+    auto coeffs = new Complex!float[half];
+
+    int hops = 0;
+    foreach(n; 0..N)
+    {
+        if (fft.feed(diff[n], coeffs))
+        {
+            for (int c = 0; c < half; ++c)
+            {
+                temp[hops, half-1-c] = coeff2Color(coeffs[c]);
+            }
+            hops++;
+        }
+    }
+
+    if (!quiet)
+    {
+        cwriteln;
+        cwritef(" * Resize to %s x %s", sw, sh);
+    }
+
+    // TODO: log-frequency
+
+    OwnedImage!RGBA spectrumImage = new OwnedImage!RGBA(sw, sh);
+    ImageResizer resizer;
+    resizer.resizeImageGeneric(temp.toRef, spectrumImage.toRef);
+
+    if (!quiet)
+    {
+        cwriteln;
+        cwritef(" * Convert to PNG");
+    }
+    ubyte[] png = convertImageRefToPNG(spectrumImage.toRef());
+    scope(exit) freeSlice(png);
+    std.file.write(spectrogramPath, png);
+
+    if (!quiet) writeln;
+}
+
+ static immutable RGBA[8] dB_COLORS =
+ [
+    RGBA(255, 255, 255, 255), // 0db => white
+    RGBA(255, 0,     0, 255), // -20db => red
+    RGBA(255, 255,   0, 255), // -40db => yellow
+    RGBA(0,   255,   0, 255), // -60db => green
+    RGBA(0,   255, 255, 255), // -80db => cyan
+    RGBA(0,     0, 255, 255), // -100db => blue
+    RGBA(255,   0, 255, 255), // -120db => magenta
+    RGBA(255,   0, 255, 255), // -140db or below => black
+];
+
+RGBA coeff2Color(Complex!float c)
+{
+    double len = abs(c);
+    double dB = -convertLinearGainToDecibel!double(len);
+    if (dB < 0) dB = 0;
+    if (dB > 139.99f) dB = 139.99f;
+    dB /= 20.0;
+    int icol = cast(int)dB;
+    ubyte t = cast(ubyte)( (dB - icol) * 256.0 );
+    return blendColor(dB_COLORS[icol+1], dB_COLORS[icol], t);
 }
