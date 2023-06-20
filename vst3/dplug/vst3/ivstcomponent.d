ad495da1352ee1b4ca85d7f8d0b4cf41569cf9b4
Guillaume Piolat <contact@auburnsounds.com>
2023-06-20 02:10:22 +0200

    Makes it clear that dplug:vst3 is dual-licensed, the dual-licence here make it subject to the same conditions as the Steinberg Agreement (which is strictly more rights that before). Comply with latest naming obligations from Steinberg agreement.

--- vst3/dplug/vst3/ivstcomponent.d
+++ vst3/dplug/vst3/ivstcomponent.d
@@ -3,11 +3,28 @@
 // (c) 2005, Steinberg Media Technologies GmbH, All Rights Reserved
 // (c) 2018, Guillaume Piolat (contact@auburnsounds.com)
 //-----------------------------------------------------------------------------
-// 
-// This Software Development Kit is licensed under the terms of the General 
+//
+// This Software Development Kit is licensed under the terms of the General
 // Public License (GPL) Version 3.
-// 
+//
+// This source is part of the "Auburn Sounds (Guillaume Piolat) extension to the 
+// Steinberg VST 3 Plug-in SDK".
+//
 // Details of that license can be found at: www.gnu.org/licenses/gpl-3.0.html
+//
+// Dual-licence:
+// 
+// The "Auburn Sounds (Guillaume Piolat) extension to the Steinberg VST 3 Plug-in
+// SDK", hereby referred to as DPLUG:VST3, is a language translation of the VST3 
+// SDK suitable for usage in Dplug. Any Licensee of a currently valid Steinberg 
+// VST 3 Plug-In SDK Licensing Agreement (version 2.2.4 or ulterior, hereby referred
+// to as the AGREEMENT), is granted by Auburn Sounds (Guillaume Piolat) a non-exclusive, 
+// worldwide, nontransferable license during the term the AGREEMENT to use parts
+// of DPLUG:VST3 not covered by the AGREEMENT, as if they were originally 
+// inside the Licensed Software Developer Kit mentionned in the AGREEMENT. 
+// Under this licence all conditions that apply to the Licensed Software Developer 
+// Kit also apply to DPLUG:VST3.
+//
 //-----------------------------------------------------------------------------
 module dplug.vst3.ivstcomponent;
 
