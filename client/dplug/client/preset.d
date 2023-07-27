9c536c38ef1795267d3d81948bf8b3f0ead13c48
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-07-27 17:26:45 +0200

    Unsupported futureBinState asserts

--- client/dplug/client/preset.d
+++ client/dplug/client/preset.d
@@ -40,6 +40,15 @@ else enum int DPLUG_SERIALIZATION_MAJOR_VERSION = 0;
 /// that is significant enough to bump a version number
 enum int DPLUG_SERIALIZATION_MINOR_VERSION = 0;
 
+// Compilation failures for futureBinState in combinations that don't work (yet!)
+version(futureBinState) {
+    version(LV2) static assert(0, "futureBinState is currently not supported for LV2 plugins!");
+    
+    version(VST2) {
+        version(legacyVST2Chunks) static assert(0, "futureBinState requires futureVST2Chunks for VST2!");
+    }
+}
+
 /// A preset is a slot in a plugin preset list
 final class Preset
 {
