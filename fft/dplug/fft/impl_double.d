a46990333d2e528428af1ae10c5ebe1987882d96
Guillaume Piolat <contact@auburnsounds.com>
2023-12-17 21:29:50 +0100

    Delete more useless code.

--- fft/dplug/fft/impl_double.d
+++ fft/dplug/fft/impl_double.d
@@ -1,4 +1,5 @@
 //          Copyright Jernej Krempuš 2012
+//          Copyright Guillaume Piolat 2016-2023
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -6,42 +7,10 @@
 module dplug.fft.impl_double;
 import dplug.fft.fft_impl;
 
-//version = SSE_AVX; // only backend supported in this fork
-
-version(SSE_AVX)
-{
-    import sse = pfft.sse_double, avx = pfft.avx_double, pfft.detect_avx;  
-    
-    alias get implementation;
-    alias TypeTuple!(FFT!(sse.Vector, sse.Options), avx) FFTs;
-}
-else
-{
-    version(Scalar)
-    {
-        import dplug.fft.scalar_double;
-    }
-    else version(Neon)
-    {
-        import dplug.fft.neon_double;
-    }
-    else version(StdSimd)
-    {
-        import dplug.fft.stdsimd;
-    }
-    else version(AVX)
-    {
-        import dplug.fft.avx_double;
-    }
-    else
-    {
-        import dplug.fft.sse_double;
-    }
-    
-    alias FFT!(Vector,Options) F;
-    alias TypeTuple!F FFTs;
-    enum implementation = 0;
-}
+import dplug.fft.sse_double;
+alias FFT!(Vector,Options) F;
+alias TypeTuple!F FFTs;
+enum implementation = 0;
 
 mixin Instantiate!();
 
--- fft/dplug/fft/impl_float.d
+++ fft/dplug/fft/impl_float.d
@@ -1,4 +1,5 @@
 //          Copyright Jernej Krempuš 2012
+//          Copyright Guillaume Piolat 2016-2023
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
@@ -6,41 +7,9 @@
 module dplug.fft.impl_float;
 import dplug.fft.fft_impl;
 
-//version = SSE_AVX; // only version supported by this fork
-
-version(SSE_AVX)
-{
-    import sse = pfft.sse_float, avx = pfft.avx_float, pfft.detect_avx;  
-    
-    alias get implementation;
-    alias TypeTuple!(FFT!(sse.Vector, sse.Options), avx) FFTs;
-}
-else
-{
-    version(Scalar)
-    {
-        import dplug.fft.scalar_float;
-    }
-    else version(Neon)
-    {
-        import dplug.fft.neon_float;
-    }
-    else version(StdSimd)
-    {
-        import dplug.fft.stdsimd;
-    }
-    else version(AVX)
-    {
-        import dplug.fft.avx_float;
-    }
-    else
-    {
-        import dplug.fft.sse_float;
-    }
-    
-    alias FFT!(Vector,Options) F;
-    alias TypeTuple!F FFTs;
-    enum implementation = 0;
-}
+import dplug.fft.sse_float;
+alias FFT!(Vector,Options) F;
+alias TypeTuple!F FFTs;
+enum implementation = 0;
 
 mixin Instantiate!();
