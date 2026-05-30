d9144ca846e7add5731fc604b68d3076fc1375f5
Guillaume Piolat <contact@auburnsounds.com>
2026-05-30 15:55:44 +0200

    Review and improve file.d

--- core/dplug/core/file.d
+++ core/dplug/core/file.d
@@ -13,32 +13,50 @@ import dplug.core.nogc;
 nothrow:
 @nogc:
 
-/// Replacement for `std.file.read`.
-/// Returns: File contents, allocated with malloc. `null` on error.
-/// Note: the result file gets an additional terminal zero after the slice, so that 
-/// it can be converted to a C string at no cost. 
-// FUTURE: this should be depreciated in favor of take a char*, this is confusing
+/** 
+    Replacement for `std.file.read`.
+
+    Params:
+        fileNameZ = A zero-terminated path.
+
+    Returns: File content, allocated with `malloc`. `null` on error.
+             The resulting slice gets an additional terminal zero byte
+             ('\0') after the slice, so that it can be converted to a 
+             C string at no cost.
+
+             `null` on error.
+
+    WARNING: `fileNameZ` MUST be followed by a terminal zero byte ('\0').
+*/
 ubyte[] readFile(const(char)[] fileNameZ)
 {
-    // assuming that fileNameZ is zero-terminated, since it will in practice be
-    // a static string
+    // FUTURE: this should be depreciated in favor of take a char*, 
+    // this is confusing
     FILE* file = fopen(assumeZeroTerminated(fileNameZ), "rb".ptr);
     if (file)
     {
         scope(exit) fclose(file);
 
-        // finds the size of the file
-        fseek(file, 0, SEEK_END);
+        // Finds the size of the file
+        if (fseek(file, 0, SEEK_END) != 0)
+            return null;
+
         long size = ftell(file);
-        fseek(file, 0, SEEK_SET);
+        if (size < 0)
+            return null;
+
+        if (fseek(file, 0, SEEK_SET) != 0)
+            return null;
 
         // Is this too large to read? 
-        // Refuse to read more than 1gb file (if it happens, it's probably a bug).
+        // Refuse to read more than 1gb file 
+        // (if it happens, it's probably a bug).
         if (size > 1024*1024*1024)
             return null;
 
         // Read whole file in a mallocated slice
-        ubyte[] fileBytes = mallocSliceNoInit!ubyte(cast(int)size + 1); // room for one additional '\0' byte
+        // +1 for one additional '\0' byte
+        ubyte[] fileBytes = mallocSliceNoInit!ubyte(cast(int)size + 1); 
         size_t remaining = cast(size_t)size;
 
         ubyte* p = fileBytes.ptr;
@@ -62,6 +80,7 @@ ubyte[] readFile(const(char)[] fileNameZ)
     else
         return null;
 }
+///ditto
 ubyte[] readFile(const(char)* fileNameZ)
 {
     import core.stdc.string: strlen;
@@ -69,12 +88,21 @@ ubyte[] readFile(const(char)* fileNameZ)
 }
 
 
-/// Replacement for `std.file.write`.
-/// Returns: `false` on error.
+/** 
+    Replacement for `std.file.write`.
+
+    Params:
+        fileNameZ = A zero-terminated path.
+        bytes = Content for the file to-be.
+
+    Returns: false on error. File may exist or not in this case.
+
+    WARNING: `fileNameZ` MUST be followed by a terminal zero byte ('\0').
+*/
 bool writeFile(const(char)[] fileNameZ, const(ubyte)[] bytes)
 {
-    // assuming that fileNameZ is zero-terminated, since it will in practice be
-    // a static string
+    // assuming that fileNameZ is zero-terminated, since it will in 
+    // practice be a static string
     FILE* file = fopen(assumeZeroTerminated(fileNameZ), "wb".ptr);
     if (file)
     {
@@ -85,4 +113,10 @@ bool writeFile(const(char)[] fileNameZ, const(ubyte)[] bytes)
     }
     else
         return false;
+}
+///ditto
+bool writeFile(const(char)* fileNameZ, const(ubyte)[] bytes)
+{
+    import core.stdc.string: strlen;
+    return writeFile(fileNameZ[0..strlen(fileNameZ)], bytes);
 }
